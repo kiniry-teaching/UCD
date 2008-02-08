8b3cc55bbd6c1d975e003483684803d6a0d9a7e8
kiniry <kiniry@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-02-08 18:36:09 +0000

    A snapshot of my RCS experiments.

---
+++ COMP20050-2008/users/kiniry/homeworks/version_control/RCS/f.c
@@ -0,0 +1,11 @@
+/* A Hello, World program, by $Author: jkiniry $ */
+
+#include <stdio.h>
+
+static char rcsid[] = "$Id: f.c 2488 2005-02-14 21:40:27Z jkiniry $";
+
+int main(int argc, char **argv) {
+  printf("Hello, World!\n");
+}
+
+  
