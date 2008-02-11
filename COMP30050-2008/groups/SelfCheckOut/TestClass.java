f64ff62c30c540e7c0e094f25dad954e7effe80d
PeterGibney <PeterGibney@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-02-11 22:47:49 +0000

    // Just fooling around to see how version works, Peter Gibney 22:47 11/02/08

--- COMP30050-2008/groups/SelfCheckOut/TestClass.java
+++ COMP30050-2008/groups/SelfCheckOut/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	/**
 	 * @param args
 	 */
+	// Just fooling around to see how version works, Peter Gibney 22:47 11/02/08
 	public static void main(Str ing[] args) {
 		// TODO Auto-generated method stub
 
