58f6709a67293fd6f84987f23f53fea3a2ed7504
emerson <emerson@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-02-12 16:20:37 +0000

    Imported some stuff.

---
+++ COMP20050-2008/users/emerson/version_control/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * A simple class that prints out a "Hello World" message on the screen.
+ *
+ * @author Emerson Loureiro (emerson.loureiro@ucd.ie)
+ */
+public class HelloWorld {
+    // Main method called when the program starts...
+    public static void main(String[] args] {
+	// Printing some stuff on the screen...
+	System.out.println("Hello World!!!");
+    }
+}
