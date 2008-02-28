f768e25ae7cb246c6222c023cecd2ca3f91cc279
roger <roger@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-02-28 12:41:30 +0000

    Snapshot

---
+++ COMP20050-2008/users/roger/homeworks/version_control/RCS/hello.java
@@ -0,0 +1,12 @@
+//$Id: hello,v 1.1 2008/02/25 16:14:13 roger Exp roger $
+public class hello {
+	public static void main (String args[]) {
+		
+		String readout = "Hello there, this is a simple java program to test a java file using RCS and the id marker";
+		
+		System.out.println(readout);
+
+		System.exit(0);
+	}
+}
+
