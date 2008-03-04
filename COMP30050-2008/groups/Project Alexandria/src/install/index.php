2832c32a66cf6d364c3f90755e4ac71ac91c5678
Ryan <Ryan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-04 21:29:24 +0000

    Began writing installation function

--- COMP30050-2008/groups/Project Alexandria/src/install/index.php
+++ COMP30050-2008/groups/Project Alexandria/src/install/index.php
@@ -3,11 +3,60 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled 1</title>
+<title>Installation</title>
+<style type="text/css">
+	form{
+		width:18em;
+	}
+	
+	.formrow{
+		width:18em;
+		margin-bottom:2px;
+	}
+	
+	.formtext{
+		width:9em;
+		display:inline-block;
+	}
+	
+	.forminput{
+		width:9em;
+		display:inline-block;
+	}
+	
+	.formbutton{
+		width:9em;
+	}
+</style>
 </head>
 
 <body>
-<?php //TODO Add install process ?>
+	<h1>Installation</h1>
+	<ol>
+		<li>Create a database on your server.</li>
+		<li>Enter the below details.</li>
+		<li>Click submit and wait for welcome message.</li>
+	</ol>
+	<h2>Note:</h2>
+	<ul>
+		<li>There's a good chance you won't have to change "localhost".</li>
+		<li>Username and password may be optional.</li>
+		<li>For further information contact your host.</li>
+	</ul>
+	<form action="index.php" method="post">
+			<div class="formrow"><div class="formtext">Host: </div><div class="forminput"><input type="text" name="host" value="localhost" /></div></div>
+			<div class="formrow"><div class="formtext">Database Username: </div><div class="forminput"><input type="text" name="username" /></div></div>
+			<div class="formrow"><div class="formtext">Database Password: </div><div class="forminput"><input type="password" name="password" /></div></div>
+			<div class="formrow"><div class="formtext">Database Name: </div><div class="forminput"><input type="text" name="dbname" /></div></div>
+			<div class="formbutton"><input type="submit" /></div>
+	</form>
+	<?php
+		if($_POST["dbname"] != NULL){
+			include("install_functions.php");
+			install($host, $username, $password, $dbname);
+		}
+	?>
+	<?php //TODO - Thomas - Add user-system install process ?>
 </body>
 
 </html>
