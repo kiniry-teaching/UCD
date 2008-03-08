6fdea1accdae4077c10be63a802d0397063842c3
Ryan <Ryan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-08 14:00:52 +0000

    This version of install_functions.php actually works and still needs tables

--- COMP30050-2008/groups/Project Alexandria/src/install/install_functions.php
+++ COMP30050-2008/groups/Project Alexandria/src/install/install_functions.php
@@ -8,17 +8,29 @@
 function install($host, $username, $password, $dbname){
 	$file = fopen("../connection.php","w");
 	echo fwrite($file,"
-		\$dbh=mysql_connect ($host, $username, $password)
+	<?php
+		\$con=mysql_connect ($host, $username, $password)
 			or die
 				('I cannot connect to the database because: ' . mysql_error());
-		mysql_select_db ($dbname);");
+		mysql_select_db ($dbname, \$con);
+	?" . ">");
 	fclose($file);
 
 	chmod("../connection.php",0644);
 	
+	$con=mysql_connect ($host, $username, $password)
+			or die
+				('I cannot connect to the database because: ' . mysql_error());
+	mysql_select_db ($dbname, $con);
+	
+	$sql = "CREATE TABLE books 
+	(
+		isbn int(13)
+	)";
+	mysql_query($sql,$con);
 	//TODO - Ryan - Create tables
 	//TODO - Thomas - Create tables	
 	
 	echo "<p>Installation Complete</p>";
 }
-?> 
+?>
