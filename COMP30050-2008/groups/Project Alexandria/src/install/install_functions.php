74cd89d8796d0db0bd2f455e86de50e9a126d089
Ryan <Ryan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-08 12:50:28 +0000

    Woking installer except for creating tables

--- COMP30050-2008/groups/Project Alexandria/src/install/install_functions.php
+++ COMP30050-2008/groups/Project Alexandria/src/install/install_functions.php
@@ -8,7 +8,7 @@
 function install($host, $username, $password, $dbname){
 	$file = fopen("../connection.php","w");
 	echo fwrite($file,"
-		$dbh=mysql_connect ($host, $username, $password)
+		\$dbh=mysql_connect ($host, $username, $password)
 			or die
 				('I cannot connect to the database because: ' . mysql_error());
 		mysql_select_db ($dbname);");
