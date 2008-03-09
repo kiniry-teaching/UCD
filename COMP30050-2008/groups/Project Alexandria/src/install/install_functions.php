4f33da2d9ff63ee11a2023a2f92e53120824d4dc
Ryan <Ryan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-09 10:43:10 +0000

    Milestones reached: 1. A working script that will create all necessary tables into a user defined database 2. All pages created

--- COMP30050-2008/groups/Project Alexandria/src/install/install_functions.php
+++ COMP30050-2008/groups/Project Alexandria/src/install/install_functions.php
@@ -1,11 +1,22 @@
 ﻿<?php
-/*
- * install() - 
- * takes the host name, database username and password and database name
- * and use them to access the database and create the tables necessary for
- * the software to function.
- */
+/****************************************************************
+* 																*
+* install() - 													*
+* takes the host name, database username and password and		*
+* database name and use them to access the database and create	*
+* the tables necessary for the software to function.			*
+* 																*
+****************************************************************/
+
 function install($host, $username, $password, $dbname){
+
+	/****************************************************************
+	* 																*
+	* Edits the connection.php file with the connection data and	*
+	* function that will be used through out the software.			*
+	* 																*
+	****************************************************************/
+
 	$file = fopen("../connection.php","w");
 	fwrite($file,"
 	<?php
@@ -17,21 +28,99 @@ function install($host, $username, $password, $dbname){
 	fclose($file);
 
 	chmod("../connection.php",0644);
-	
-	$con=mysql_connect ($host, $username, $password)
-			or die
-				('I cannot connect to the database because: ' . mysql_error());
+
+	/****************************************************************
+	* 																*
+	* Connects to the database										*
+	* 																*
+	****************************************************************/
+	 	 	
+	$con=mysql_connect ($host, $username, $password);
+	if (!$con){
+		die('Could not connect: ' . mysql_error());
+	}
 	mysql_select_db ($dbname, $con);
+
+	/****************************************************************
+	* 																*
+	* Creates all the book related tables							*
+	* 																*
+	****************************************************************/
+
+	$sql = 'DROP TABLE IF EXISTS `books`';
+	mysql_query($sql,$con);	
 	
 	$sql = "CREATE TABLE books 
 	(
-		isbn int(13)
+		isbn int(13) NOT NULL, 
+		PRIMARY KEY(isbn),
+		title tinytext,
+		titleLong text,
+		author tinytext,
+		publisher tinytext,
+		ddc decimal,
+		description text
 	)";
-	mysql_query($sql,$con);
-	//TODO - Ryan - Create tables
+	mysql_query($sql,$con);	
+	
+	$sql = 'DROP TABLE IF EXISTS `books_onloan`';
+	mysql_query($sql,$con);	
+
+	$sql = "CREATE TABLE books_onloan 
+	(
+		loanNo bigint NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(loanNo),
+		isbnUsername text,
+		date int
+	)";
+	mysql_query($sql,$con);	
+	
+	$sql = 'DROP TABLE IF EXISTS `books_requests`';
+	mysql_query($sql,$con);	
+
+	$sql = "CREATE TABLE books_requests 
+	(
+		requestNo bigint NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(requestNo),
+		isbnUsername text
+	)";
+	mysql_query($sql,$con);	
+	
+	$sql = 'DROP TABLE IF EXISTS `books_returned`';
+	mysql_query($sql,$con);	
+	
+	$sql = "CREATE TABLE books_returned 
+	(
+		returnNo bigint NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(returnNo),
+		isbnUsername text
+	)";
+	mysql_query($sql,$con);	
+	
+	$sql = 'DROP TABLE IF EXISTS `books_reviews`';
+	mysql_query($sql,$con);	
+	
+	$sql = "CREATE TABLE books_review 
+	(
+		reviewNo bigint NOT NULL AUTO_INCREMENT, 
+		PRIMARY KEY(reviewNo),
+		isbnUsername text,
+		rating int(5),
+		review text
+	)";
+	mysql_query($sql,$con);	
+
 	//TODO - Thomas - Create tables	
 	
-	mysql_close($con);
-	echo "<p>Installation Complete</p>";
+	/****************************************************************
+	* 																*
+	* Ends the installation.										*
+	* 																*
+	****************************************************************/
+	
+	if ($con){		
+		mysql_close($con);
+		echo "<p>Installation Complete</p>";
+	}
 }
 ?>
