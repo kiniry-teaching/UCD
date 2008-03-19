d6815ba188ece5f362d1fb20a1acde44d6b30b6c
eoin <eoin@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-19 13:36:05 +0000

    Refactored to allow for immutable models of each type

--- COMP30050-2008/groups/Infidus/Drought/Drought/Graphics/Model3D.cs
+++ COMP30050-2008/groups/Infidus/Drought/Drought/Graphics/Model3D.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
@@ -10,39 +11,38 @@ namespace Drought.Graphics
 {
     public class Model3D
     {
-        public Camera camera;
+        private static Dictionary<string,Model3D> instances = new Dictionary<string,Model3D>();
+
+        private static Camera camera;
 
         private static Effect effect;
 
         private static GraphicsDevice graphics;
 
-        string modelName;
+        private string modelName;
 
-        Model model;
+        private Model model;
 
-        Texture2D[] textures;
+        private Texture2D[] textures;
 
-        public Vector3 position;
+        private Matrix projectionMatrix;
 
-        public Vector3 rotationAngles;
-
-        public Vector3 scaleFactors;
-
-        Matrix projectionMatrix;
-
-        public Model3D(string modelName, Camera camera)
+        private Model3D(string modelName, Camera camera, ContentManager content, GraphicsDevice graphics)
         {
             this.modelName = modelName;
             this.camera = camera;
 
-            rotationAngles = new Vector3(0.0f);
-            scaleFactors = new Vector3(1.0f);
-
-            position = camera.getPosition(); 
             projectionMatrix = camera.getProjectionMatrix();
+
+            loadContent(content, graphics);
         }
 
-        public void loadContent(ContentManager content, GraphicsDevice graphics)
+        public Model3D getInstance(string model, Camera camera)
+        {
+
+        }
+
+        private void loadContent(ContentManager content, GraphicsDevice graphics)
         {
             if (effect == null)
                 effect = content.Load<Effect> ("EffectFiles/model");
@@ -65,30 +65,7 @@ namespace Drought.Graphics
                     meshPart.Effect = effect.Clone(graphics);
         }
 
-        public void render(GraphicsDevice graphics)
-        {
-            int i = 0;
-            foreach (ModelMesh mesh in model.Meshes)
-            {
-                foreach (Effect currentEffect in mesh.Effects)
-                {
-                    currentEffect.CurrentTechnique = effect.Techniques["Textured"];
-
-                    Matrix worldMatrix = Matrix.CreateRotationX(rotationAngles.X) * Matrix.CreateRotationY(rotationAngles.Y) * Matrix.CreateRotationZ(rotationAngles.Z) *
-                        Matrix.CreateScale(scaleFactors) * 
-                        Matrix.CreateTranslation(position);
-
-                    currentEffect.Parameters["xWorld"].SetValue(worldMatrix);
-                    currentEffect.Parameters["xView"].SetValue(camera.getViewMatrix());
-                    currentEffect.Parameters["xProjection"].SetValue(projectionMatrix);
-                    currentEffect.Parameters["xTexture"].SetValue(textures[i++]);
-                }
-                mesh.Draw();
-            }
-
-        }
-
-        public void render(GraphicsDevice graphics, Matrix orientation, Vector3 position)
+        public void render(GraphicsDevice graphics, Matrix orientation, Vector3 position, Vector2 scaleFactors)
         {
             Matrix worldMatrix = orientation * Matrix.CreateTranslation(position) * Matrix.CreateScale(scaleFactors);
 
