c53b6c5b2bbb37fd51320bff7a46e6ddf393975c
Ryan <Ryan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-21 17:36:39 +0000

    Forgot to add these

---
+++ COMP30050-2008/groups/Project Alexandria/src/include/variables.php
@@ -0,0 +1,22 @@
+<?php
+/****************************************************************
+* 																*
+* Global variabes needed for adding, editing and retrieval of	*
+* books															*
+* 																*
+****************************************************************/
+$isbn = "";
+$title = "";
+$titleLong = "";
+$authors = "";
+$publisher = "";
+$noOfPages = "";
+$binding = "";
+$ddc = "";
+$lcc = "";
+$description = "";
+$largeImg = "";
+$mediumImg = "";
+$smallImg = "";
+$noOfCopies = "";
+?>
---
+++ COMP30050-2008/groups/Project Alexandria/src/include/variables_global.php
@@ -0,0 +1,23 @@
+<?php
+/****************************************************************
+* 																*
+* Declares variabes needed for adding, editing and retrieval of	*
+* books	as their global											*
+* 																*
+****************************************************************/
+global $isbn,
+	$title,
+	$titleLong,
+	$authors,
+	$publisher,
+	$noOfPages,
+	$binding,
+	$ddc,
+	$lcc,
+	$description,
+	$largeImg,
+	$mediumImg,
+	$smallImg,
+	$bookData,
+	$noOfCopies;
+?>
