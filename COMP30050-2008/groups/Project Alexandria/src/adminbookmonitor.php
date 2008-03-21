92c04aa3a5d693855ea48b003ee48bb564685ef6
Ryan <Ryan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-21 15:23:11 +0000

    adminbookmonitor.php finished.

--- COMP30050-2008/groups/Project Alexandria/src/adminbookmonitor.php
+++ COMP30050-2008/groups/Project Alexandria/src/adminbookmonitor.php
@@ -21,11 +21,19 @@ $result = mysql_query("SELECT * FROM books_onloan
 	ORDER BY date");
 
 echo "<div class='onloan_table'>";
+	echo "<div class='onloan_row_header'>";
+		echo "<div class='onloan_isbn_header'>ISBN</div>";
+		echo "<div class='onloan_username_header'>Username</div>";
+		echo "<div class='onloan_date_header'>Date</div>";
+		echo "<div class='onloan_returned_header'>Returned</div>";
+		echo "<div class='onloan_lost_header'>Lost</div>";
+		echo "<div class='onloan_renew_header'>Renew</div>";
+	echo "</div>";
 while($row = mysql_fetch_array($result)){
 	echo "<div class='onloan_row'>";
 		echo "<div class='onloan_isbn'>" . $row['isbn'] . "</div>";
 		echo "<div class='onloan_username'>" . $row['username'] . "</div>";
-		echo "<div class='onloan_date'>" . $row['date'] . "</div>";
+		echo "<div class='onloan_date'>" . date("d/m/y", $row['date']) . "</div>";
 		echo "<div class='onloan_returned'><a href='adminbookmonitor.php?state=0&isbn=" . $row['isbn'] ."&username=" . $row['username'] . "'>Returned</a></div>";
 		echo "<div class='onloan_lost'><a href='adminbookmonitor.php?state=1&isbn=" . $row['isbn'] ."&username=" . $row['username'] . "'>Lost</a></div>";
 		echo "<div class='onloan_renew'><a href='adminbookmonitor.php?state=2&isbn=" . $row['isbn'] ."&username=" . $row['username'] . "&date=" . time() . "'>Renew</a></div>";
@@ -38,9 +46,15 @@ echo "</div>";
 <?php
 include('connection.php');
 $result = mysql_query("SELECT * FROM books_requests
-	ORDER BY date");
+	ORDER BY no");
 
 echo "<div class='requests_table'>";
+	echo "<div class='requests_row_header'>";
+		echo "<div class='requests_isbn_header'>ISBN</div>";
+		echo "<div class='requests_username_header'>Username</div>";
+		echo "<div class='requests_loaned_header'>Loaded</div>";
+		echo "<div class='requests_delete_header'>Delete</div>";
+	echo "</div>";
 while($row = mysql_fetch_array($result)){
 	echo "<div class='requests_row'>";
 		echo "<div class='requests_isbn'>" . $row['isbn'] . "</div>";
