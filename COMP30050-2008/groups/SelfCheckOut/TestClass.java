39c2ebe3194aee9b261b33db1de3f3d36082097f
PeterGibney <PeterGibney@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-03-31 16:46:45 +0000

    Changed file to see if versioning working for me again.  Peter5

--- COMP30050-2008/groups/SelfCheckOut/TestClass.java
+++ COMP30050-2008/groups/SelfCheckOut/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	 * @param args
 	 */
 	//Just fooling around to see how version works, Peter Gibney 22:47 11/02/08
-	public static void main(Str ing[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
