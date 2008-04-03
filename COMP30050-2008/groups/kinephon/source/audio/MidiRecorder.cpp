5f171fa7040a3b86c02d4042dda8cf4f3e3784c4
evka <evka@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-03 09:50:48 +0000

    changes

--- COMP30050-2008/groups/kinephon/source/audio/MidiRecorder.cpp
+++ COMP30050-2008/groups/kinephon/source/audio/MidiRecorder.cpp
@@ -12,6 +12,7 @@ MidiRecorder::~MidiRecorder() {}
 
 bool MidiRecorder::openFile(int noTracks) {
     myFile_.open("writeFile.mid", ios::out | ios::binary);
+    
     /*header chunk looks like: 
       4D 54 68 64 (MThd) 00 00 00 06 (FIXED) ff ff nn nn dd dd
       # ff ff is the file format. There are 3 formats:
@@ -22,14 +23,10 @@ bool MidiRecorder::openFile(int noTracks) {
     char header_first[8] = { 'M', 'T', 'h', 'd', 0, 0, 0, 6 };
     myFile_.write(header_first, 8);
     
-    //this will vary
-    //TODO: determine the best delta tick time
-    char header_second[6] = { 0, 0, 0, 1, 0, 72 }; 
+    char header_second[6] = { 0, 0, 0, 1,0, 0x60 }; 
     myFile_.write(header_second, 6);
     
-    //don't write the track header, since we could only write half of it anyway...
-
-    return false;
+    return true;
 }
 
 bool MidiRecorder::openFile(string fileName, int noTracks) {
@@ -38,28 +35,34 @@ bool MidiRecorder::openFile(string fileName, int noTracks) {
 }
 
 bool MidiRecorder::closeFile() {
-    //have to write buffer into file with all the missing header information
     /*track chunks, header:
-      4D 54 72 6B (MTrk) xx xx xx xx (lenght of track not including header in bytes)
-     
+      4D 54 72 6B (MTrk) xx xx xx xx (lenght of track not including header in bytes) 
      */
     char track_first[4] = { 'M', 'T', 'r', 'k' };
+    char end_of_track[4] = {0x00, 0xFF, 0x2F, 0x00};//absolutely necessary
+    char time_signature[7] = {0xFF, 0x58, 0x04, 0x04, 0x02, 0x24, 0x08};//opt 
+    char tempo[7] = {0x00, 0xFF, 0x51, 0x03, 0x07, 0xA1, 0x20};//opt
+    char track_name[7] = { 0xFF, 0x03, 0x04 , 't', 'e', 's', 't'};//opt
+ 
+    
+    //TODO: determine size if track in right format
+    char track_size[4] = { 0, 0, 0, 76};
+     
     myFile_.write(track_first, 4);
-    //determine size of buffer in bytes and covert them into char size chunks
-    char track_size[4] = { 0, 0, 0, 0};
-    //TODO: this needs work 
-    track_size[3] = trackOne_.size() % 128;
-    track_size[2] = (trackOne_.size() % 16384) - track_size[3];
-    track_size[1] = (trackOne_.size() % 2097152) - track_size[2] - track_size[3];
-    track_size[0] = trackOne_.size() >> 12;
     myFile_.write(track_size, 4);
     
-    //need a vector, not an array, so take the address of the first element, ugly but don't want to copy the whole thing
-    //and we need the array of chars not unsigned ones
-    myFile_.write((char*)&trackOne_[0], trackOne_.size());
-     
+   // myFile_.write(track_name, 7);
+    myFile_.write(time_signature, 7);
+    myFile_.write(tempo, 7);
+    
+    
+    myFile_.write((char*)&trackOne_[0], 55);//actual data
+    myFile_.write(end_of_track, 4);
     myFile_.close();
-    return false;
+    
+    //cout << "real track size: " << trackOne_.size() << endl;
+    //cout << "track size: " << track_size[0] << " " << track_size[1] << " " << track_size[2] << " "<< track_size[3] << endl; 
+    return true;
 }
 
 bool MidiRecorder::write(vector<uchar> event, int trackNo) {
@@ -74,9 +77,11 @@ bool MidiRecorder::write(vector<uchar> event, int trackNo) {
         
         
     //add midi event data:
-    for (int i = 0; i < event.size(); i++)
+    for (uchar i = 0; i < event.size(); i++)
         trackOne_.push_back(event[i]); 
-    return false;
+    
+    cout << "writing: " << (int) event[0] << "  " << (int) event[1] << endl;
+    return true;
 }
 
 
