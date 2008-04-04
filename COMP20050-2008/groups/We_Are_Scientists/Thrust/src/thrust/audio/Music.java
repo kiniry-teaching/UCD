8193c264a46338462e202457d0669ec4fa35a289
mewtio <mewtio@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-04 17:42:23 +0000

    edit sounds

--- COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/audio/Music.java
+++ COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/audio/Music.java
@@ -1,5 +1,5 @@
 package thrust.audio;
-
+import javax.sound.sampled.*;
 /**
  * In-game music.
  * @author Joe Kiniry (kiniry@acm.org)
@@ -8,13 +8,52 @@ package thrust.audio;
 public class Music {
   //@ public model boolean is_playing;
 
+  
+  
+  
+  
+  boolean is_playing;
   /**
    * @return Is music playing?
    */
   //@ ensures \result == is_playing;
   public /*@ pure @*/ boolean playing() {
-    assert false; //@ assert false;
-    return false;
+    try {
+      // From file
+      AudioInputStream stream = AudioSystem.getAudioInputStream(new File("thrust"));
+  
+     
+  
+      // At present, ALAW and ULAW encodings must be converted
+      // to PCM_SIGNED before it can be played
+      AudioFormat format = stream.getFormat();
+      if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
+          format = new AudioFormat(
+                  AudioFormat.Encoding.PCM_SIGNED,
+                  format.getSampleRate(),
+                  format.getSampleSizeInBits()*2,
+                  format.getChannels(),
+                  format.getFrameSize()*2,
+                  format.getFrameRate(),
+                  true);        // big endian
+          stream = AudioSystem.getAudioInputStream(format, stream);
+      }
+  
+      // Create the clip
+      DataLine.Info info = new DataLine.Info(
+          Clip.class, stream.getFormat(), ((int)stream.getFrameLength()*format.getFrameSize()));
+      Clip clip = (Clip) AudioSystem.getLine(info);
+  
+      // This method does not return until the audio file is completely loaded
+      clip.open(stream);
+  
+      // Start playing
+      clip.start();
+  } catch (MalformedURLException e) {
+  } catch (IOException e) {
+  } catch (LineUnavailableException e) {
+  } catch (UnsupportedAudioFileException e) {
+  }
   }
 
   /**
@@ -22,7 +61,10 @@ public class Music {
    */
   //@ ensures is_playing;
   public void start() {
-    assert false; //@ assert false;
+    //clip.start();
+    clip.loop(Clip.LOOP_CONTINUOUSLY);
+    int numberOfPlays = 3;
+    //clip.loop(numberOfPlays-1);
   }
 
   /**
