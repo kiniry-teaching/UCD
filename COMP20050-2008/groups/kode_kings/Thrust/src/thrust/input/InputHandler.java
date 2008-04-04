e9c54ed171c5ad6866a9e61e0c57c3ae80d3058b
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-04 16:52:39 +0000

    Defined keys and implemented methods

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/input/InputHandler.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/input/InputHandler.java
@@ -6,33 +6,41 @@ package thrust.input;
  * @version 2 April 2008
  */
 public class InputHandler {
-  /** An unknown character code. */
-  private static final char UNKNOWN_CHAR = '\0';
-  /** Fill in this comment. */
-  public static final char DISPLAY_HIGH_SCORES = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char TOGGLE_MUSIC_OR_EFFECTS = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char START_GAME = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char STOP_GAME = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char FIRE_GUN = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char TURN_LEFT = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char TURN_RIGHT = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char USE_ENGINE = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char USE_SHIELD = UNKNOWN_CHAR;
+  /** Press h to Display the high score table. */
+  public static final char DISPLAY_HIGH_SCORES = 'h';
+  /** Press m to Toggle between hearing music or sound effects. */
+  public static final char TOGGLE_MUSIC_OR_EFFECTS = 'm';
+  /** Press n to Start a game. */
+  public static final char START_GAME = 'n';
+  /** Press q to stop a game. */
+  public static final char STOP_GAME = 'q';
+  /** Press . to Fire a bullet from the spaceship's gun. */
+  public static final char FIRE_GUN = '.';
+  /** Press a to Rotate the spaceship counterclockwise. */
+  public static final char TURN_LEFT = 'a';
+  /** Press s to Rotate the spaceship clockwise. */
+  public static final char TURN_RIGHT = 's';
+  /** Press , to Use the spaceship's engine to apply thrust forward. */
+  public static final char USE_ENGINE = ',';
+  /** Press space to Use the spaceships shield to protect itself or to gather fuel. */
+  public static final char USE_SHIELD = ' ';
 
   /**
    * @return What are the legal keyboard inputs?
    */
-  public /*@ pure @*/ char[] legal_inputs() {
-    assert false; //@ assert false;
-    return null;
+  public /*@ pure @*/ char[] legal_inputs() {   
+    char[] legal_inputs = new char[9];
+    assert legal_inputs != null; //@ assert legal_inputs != null;
+    legal_inputs[0] = DISPLAY_HIGH_SCORES;
+    legal_inputs[1] = TOGGLE_MUSIC_OR_EFFECTS;
+    legal_inputs[2] = START_GAME;
+    legal_inputs[3] = STOP_GAME;
+    legal_inputs[4] = FIRE_GUN;
+    legal_inputs[5] = TURN_LEFT;
+    legal_inputs[6] = TURN_RIGHT;
+    legal_inputs[7] = USE_ENGINE;
+    legal_inputs[8] = USE_SHIELD;
+    return legal_inputs;
   }
 
   /**
@@ -49,8 +57,16 @@ public class InputHandler {
     @                      (the_character == USE_ENGINE) |
     @                      (the_character == USE_SHIELD);
     @*/
-  public /*@ pure @*/ boolean legal_input(char the_character) {
-    assert false; //@ assert false;
+  public /*@ pure @*/ boolean legal_input(char the_character) {   
+    char[] legal_inputs = legal_inputs();
+    assert legal_inputs != null; //@ assert legal_inputs != null;
+    for(int i = 0; i < legal_inputs.length; i++)
+    {
+      if(legal_inputs[i] == the_character)
+      {
+        return true;
+      }     
+    }
     return false;
   }
 
