12ecb237998b7f0edaa75d301ba2f6de05941e34
timbyr <timbyr@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-04 09:05:52 +0000

    Possible SoundEffects Class

--- COMP20050-2008/groups/Jar/Thrust/src/thrust/audio/SoundEffect.java
+++ COMP20050-2008/groups/Jar/Thrust/src/thrust/audio/SoundEffect.java
@@ -1,27 +1,58 @@
-package thrust.audio;
+ppackage thrust.audio;
 
 import java.io.File;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
 /**
  * Any sound made in response to a event.
+ * 
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 2 April 2008
  */
 public class SoundEffect {
+  /** Clip to be played. */
+  private Clip clip;
   /**
    * This is your sound effect.
-   * @param filename the sound effect to make.
+   * 
+   * @param filename
+   *          the sound effect to make.
    * @return the new sound effect for the effect stored in 's'.
    */
-  public final /*@ pure @*/ SoundEffect make(final File filename) {
-    //@ assert false;
-    return null;
-  }
+  public final /* @ pure @ */ SoundEffect make(final File filename) {
 
-  /**
-   * Start playing your effect.
-   */
-  public final void start() {
-    //@ assert false;
-  }
+    AudioInputStream audioInputStream = null;
+    try {
+      audioInputStream = AudioSystem.getAudioInputStream(filename);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+    AudioFormat format = audioInputStream.getFormat();
+    DataLine.Info info = new DataLine.Info(Clip.class, format);
+    try
+    {
+      clip = (Clip) AudioSystem.getLine(info);
+      clip.open(audioInputStream);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  return null;
+  // @ assert false;
+}
+
+/**
+ * Start playing your effect.
+ */
+public final void start() {
+    clip.start();
+  // @ assert false;
+}
 }
