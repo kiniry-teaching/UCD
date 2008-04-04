c8103a5cba9a2f70613769524423dd4030cf2c1a
zylius <zylius@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-04 14:59:50 +0000

    Submission attempt

--- COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/input/InputHandler.java
+++ COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/input/InputHandler.java
@@ -1,4 +1,5 @@
 package thrust.input;
+import java.awt.event.KeyEvent;
 
 /**
  * Processes and delegates each keyboard input received.
@@ -8,31 +9,40 @@ package thrust.input;
 public class InputHandler {
   /** An unknown character code. */
   private static final char UNKNOWN_CHAR = '\0';
-  /** Fill in this comment. */
-  public static final char DISPLAY_HIGH_SCORES = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char TOGGLE_MUSIC_OR_EFFECTS = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char START_GAME = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char STOP_GAME = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char FIRE_GUN = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char TURN_LEFT = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char TURN_RIGHT = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char USE_ENGINE = UNKNOWN_CHAR;
-  /** Fill in this comment. */
-  public static final char USE_SHIELD = UNKNOWN_CHAR;
+  /** Character code for displaying high scores (F12) */
+  public static final char DISPLAY_HIGH_SCORES = KeyEvent.VK_F12;
+  /** Character code for toggling sound and music (S) */
+  public static final char TOGGLE_MUSIC_OR_EFFECTS = KeyEvent.VK_S;
+  /** Character code to start the game (Enter) */
+  public static final char START_GAME = KeyEvent.VK_ENTER;
+  /** Character code to stop (pause) the game (Pause) */
+  public static final char STOP_GAME = KeyEvent.VK_PAUSE;
+  /** Character code to fire the gun (Space) */
+  public static final char FIRE_GUN = KeyEvent.VK_SPACE;
+  /** Character code to turn left (Left arrow) */
+  public static final char TURN_LEFT = KeyEvent.VK_LEFT;
+  /** Character code to turn right (Right arrow) */
+  public static final char TURN_RIGHT = KeyEvent.VK_RIGHT;
+  /** Character code to use engine (thrust) (Up arrow) */
+  public static final char USE_ENGINE = KeyEvent.VK_UP;
+  /** Character code to enable shield (Shift) */
+  public static final char USE_SHIELD = KeyEvent.VK_SHIFT;
 
   /**
    * @return What are the legal keyboard inputs?
    */
   public /*@ pure @*/ char[] legal_inputs() {
-    assert false; //@ assert false;
-    return null;
+    char legal[] = new char[9];
+    legal[0] = KeyEvent.VK_F12;
+    legal[1] = KeyEvent.VK_S;
+    legal[2] = KeyEvent.VK_ENTER;
+    legal[3] = KeyEvent.VK_PAUSE;
+    legal[4] = KeyEvent.VK_SPACE;
+    legal[5] = KeyEvent.VK_UP;
+    legal[6] = KeyEvent.VK_SHIFT;
+    legal[7] = KeyEvent.VK_LEFT;
+    legal[8] = KeyEvent.VK_RIGHT;
+    return legal;
   }
 
   /**
@@ -50,8 +60,13 @@ public class InputHandler {
     @                      (the_character == USE_SHIELD);
     @*/
   public /*@ pure @*/ boolean legal_input(char the_character) {
-    assert false; //@ assert false;
-    return false;
+    boolean retBool = false;
+    for(int i = 0; i < legal_inputs().length; ++i) {
+      if(legal_inputs()[i] == the_character) {
+        retBool = true;
+      }
+    }
+    return retBool;
   }
 
   /**
