a192304786f5dd4563c58417aa3bf1e06cea1d05
mewtio <mewtio@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-04 15:02:58 +0000

    updating

--- COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/input/InputHandler.java
+++ COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/input/InputHandler.java
@@ -1,5 +1,5 @@
 package thrust.input;
-
+import java.awt.event.KeyEvent;
 /**
  * Processes and delegates each keyboard input received.
  * @author Joe Kiniry (kiniry@acm.org)
@@ -9,30 +9,40 @@ public class InputHandler {
   /** An unknown character code. */
   private static final char UNKNOWN_CHAR = '\0';
   /** Fill in this comment. */
-  public static final char DISPLAY_HIGH_SCORES = UNKNOWN_CHAR;
+  public static final char DISPLAY_HIGH_SCORES = 'h';
   /** Fill in this comment. */
-  public static final char TOGGLE_MUSIC_OR_EFFECTS = UNKNOWN_CHAR;
+  public static final char TOGGLE_MUSIC_OR_EFFECTS = 'm';
   /** Fill in this comment. */
-  public static final char START_GAME = UNKNOWN_CHAR;
+  public static final char START_GAME = '\u00A0';
   /** Fill in this comment. */
-  public static final char STOP_GAME = UNKNOWN_CHAR;
+  public static final char STOP_GAME = '\u001B';
   /** Fill in this comment. */
-  public static final char FIRE_GUN = UNKNOWN_CHAR;
+  public static final char FIRE_GUN = '\n';
   /** Fill in this comment. */
-  public static final char TURN_LEFT = UNKNOWN_CHAR;
+  public static final char TURN_LEFT = 'a';
   /** Fill in this comment. */
-  public static final char TURN_RIGHT = UNKNOWN_CHAR;
+  public static final char TURN_RIGHT = 's';
   /** Fill in this comment. */
-  public static final char USE_ENGINE = UNKNOWN_CHAR;
+  public static final char USE_ENGINE = '\u000F';
   /** Fill in this comment. */
-  public static final char USE_SHIELD = UNKNOWN_CHAR;
+  public static final char USE_SHIELD = '\u00A0';
 
   /**
    * @return What are the legal keyboard inputs?
    */
   public /*@ pure @*/ char[] legal_inputs() {
-    assert false; //@ assert false;
-    return null;
+    char[] array = new char[9];
+    array[0] = '\0';
+    array[1] = 'h';
+    array[2] = 'm';
+    array[3] = '\u00A0';
+    array[4] = '\u001B';
+    array[5] = '\n';
+    array[6] = 'a';
+    array[7] = 's';
+    array[8] = '\u000F';
+    //assert false; //@ assert false;
+    return array;
   }
 
   /**
@@ -50,7 +60,12 @@ public class InputHandler {
     @                      (the_character == USE_SHIELD);
     @*/
   public /*@ pure @*/ boolean legal_input(char the_character) {
-    assert false; //@ assert false;
+    if((the_character == 'h') || (the_character == 'm') || (the_character == '\u00A0')
+        || (the_character == '\u001B') || (the_character == '\n') || (the_character == 'a')
+        || (the_character == 's') || (the_character == '\u000F'))
+      return true;
+    
+    //assert false; //@ assert false;
     return false;
   }
 
@@ -60,6 +75,6 @@ public class InputHandler {
    */
   //@ requires legal_input(the_keyboard_input);
   public void process(char the_keyboard_input) {
-    assert false; //@ assert false;
+    //assert false; //@ assert false;
   }
 }
