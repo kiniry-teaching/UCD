2ba34563ea8e2ac8533630fe05ffdeae3e41d031
Percy <Percy@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-04 16:18:43 +0000

    Music/SoundEffect done

--- COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/audio/Music.java
+++ COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/audio/Music.java
@@ -1,5 +1,7 @@
 package thrust.audio;
 
+import java.io.File;
+import javax.sound.sampled.*;
 /**
  * In-game music.
  * @author Joe Kiniry (kiniry@acm.org)
@@ -8,13 +10,36 @@ package thrust.audio;
 public class Music {
   //@ public model boolean is_playing;
 
+  private File musicFile = new File("../../../media/Thrust_music.wav");
+  Clip music = null;
+  
   /**
    * @return Is music playing?
    */
   //@ ensures \result == is_playing;
+  public Music(){
+     AudioInputStream audioStream = null;
+    
+    try{
+      audioStream = AudioSystem.getAudioInputStream(musicFile);
+    }  catch (Exception e){
+      e.printStackTrace();
+    }
+    
+    final AudioFormat aFormat = audioStream.getFormat();
+    final DataLine.Info info = new DataLine.Info(SourceDataLine.class, aFormat);
+  
+    try {
+      music = (Clip) AudioSystem.getLine(info);
+      music.open(audioStream);
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+    
+  }
   public /*@ pure @*/ boolean playing() {
-    assert false; //@ assert false;
-    return false;
+    return music.isRunning();
+    //@ assert false;
   }
 
   /**
@@ -22,7 +47,7 @@ public class Music {
    */
   //@ ensures is_playing;
   public void start() {
-    assert false; //@ assert false;
+    music.loop(Clip.LOOP_CONTINUOUSLY);
   }
 
   /**
@@ -30,6 +55,6 @@ public class Music {
    */
   //@ ensures !is_playing;
   public void stop() {
-    assert false; //@ assert false;
+    music.stop();
   }
 }
--- COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/audio/SoundEffect.java
+++ COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/audio/SoundEffect.java
@@ -1,6 +1,7 @@
 package thrust.audio;
 
 import java.io.File;
+import javax.sound.sampled.*;
 
 /**
  * Any sound made in response to a event.
@@ -8,20 +9,37 @@ import java.io.File;
  * @version 2 April 2008
  */
 public class SoundEffect {
+  private Clip sound = null;
   /**
    * This is your sound effect.
    * @param the_sound_effect_file the sound effect to make.
    * @return the new sound effect for the effect stored in 's'.
    */
-  public /*@ pure @*/ SoundEffect make(File the_sound_effect_file) {
-    assert false; //@ assert false;
-    return null;
+  public /*@ pure @*/ SoundEffect(File the_sound_effect_file) {
+     AudioInputStream audioStream = null;
+    
+    try{
+      audioStream = AudioSystem.getAudioInputStream(the_sound_effect_file);
+    }  catch (Exception e){
+      e.printStackTrace();
+    }
+
+    final AudioFormat aFormat = audioStream.getFormat();
+    final DataLine.Info info = new DataLine.Info(SourceDataLine.class, aFormat);
+    
+    try {
+      sound = (Clip) AudioSystem.getLine(info);
+      sound.open(audioStream);
+    }catch (Exception e){
+      e.printStackTrace();
+    }
   }
 
   /**
    * Start playing your effect.
    */
   public void start() {
+    sound.loop(1);
     assert false; //@ assert false;
   }
 }
