6ad44a775444aae3014e3506935e2fe4a33741af
daire <daire@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-05 10:25:36 +0000

    I tried to get rid of the errors, shortened names, changed array, etc

--- COMP20050-2008/groups/TEAM_FOOTPRINT/Thrust/src/thrust/input/InputHandler.java
+++ COMP20050-2008/groups/TEAM_FOOTPRINT/Thrust/src/thrust/input/InputHandler.java
@@ -1,41 +1,40 @@
 
 //class written by Daire O'Doherty 06535691 3/4/08
+
 //Just trying to set up what the legal inputs for the game are, and how the computer checks if what is pressed is legal
 
+//Daire O'Doherty 06535691 reworked on it 5/4/08, havn't been able to get in touch with one of my team members in a few weeks, other member Sinead morley
+//said that she would implement the audio, I have started a bit of the Physics myself, as i don't know if Lane is going to do it
 package thrust.input;
 
 public class InputHandler {
   
  
-  private static final char UNKNOWN_CHAR = '\0';
-    public static final char 
-      DISPLAY_HIGH_SCORES = 'h', //when h is pressed, the high scores will be displayed
-      TOGGLE_MUSIC_OR_SOUND_EFFECTS = 'm', // when m is pressed the music will be toggled
-      START_GAME = '\u0020', // when the space key is pressed the game will start
-      STOP_GAME = '\u001B', //when escape is pressed the game will stop
-      FIRE_GUN = '\r',  //when return is pressed the ship will fire
-      TURN_LEFT = 'a',  // when a is pressed the ship will turn right
-      TURN_RIGHT = 's', // when s is pressed the ship will turn right
-      USE_ENGINE = 'g', //  when g is pressed the ship will use its engine
-      USE_SHIELD = 'd'; //  when d is pressed the shield will be used
-    //couldn't find ascii or unicode for shift
+
     
+  public static final char DISPLAY_SCORES = 'h'; 
+//when h is pressed, the high scores will be displayed
+  public static final char TOGGLE_MUSIC = 'm'; 
+//when m is pressed the music will be toggled
+  public static final char START_GAME = '32'; 
+//space key is pressed the game will start
+  public static final char STOP_GAME = '27'; //when escape is pressed the game will stop
+  public static final char FIRE_GUN = '\r'; //when return is pressed the ship will fire
+  public static final char     TURN_LEFT = 'a';  // when a is pressed the ship will turn right
+  public static final char     TURN_RIGHT = 's'; // when s is pressed the ship will turn right
+  public static final char     USE_ENGINE = '15'; //  when [shift] is pressed the ship will use its engine
+  public static final char     USE_SHIELD = 'd'; //  when d is pressed the shield will be used
+  
+    //researched ASCII code online and found 32,27,15 the ones im looking for
   /**
    * @return What are the legal keyboard inputs?
    */
   public /*@ pure @*/ char[] legal_inputs() {
      //@ assert false;
     
-    char[] legal_inputs = new char[9];
-    legal_inputs[0]=DISPLAY_HIGH_SCORES;
-    legal_inputs[1] = TOGGLE_MUSIC_OR_SOUND_EFFECTS;
-    legal_inputs[2] =  START_GAME;
-    legal_inputs[3] = STOP_GAME;
-    legal_inputs[4]= FIRE_GUN;
-    legal_inputs[5]=TURN_LEFT ;
-    legal_inputs[6] = TURN_RIGHT;
-    legal_inputs[7]=USE_ENGINE;
-    legal_inputs[8]= USE_SHIELD;
+    char[] legal_inputs={DISPLAY_SCORES,TOGGLE_MUSIC,
+                           START_GAME,STOP_GAME,FIRE_GUN,TURN_LEFT,
+                           TURN_RIGHT ,USE_ENGINE, USE_SHIELD};
   
     return legal_inputs;
   }
@@ -55,7 +54,7 @@ public class InputHandler {
     @*/
   //@requires true
   public /*@ pure @*/ boolean legal_input(char k) {
-     //@ assert false;
+     
     
     //loop through the legal_inputs array
     for(int i = 0; i <= legal_inputs().length; i++)
@@ -64,7 +63,7 @@ public class InputHandler {
                                         return true; //if the character typed is the same as one of the characters in the input
                                 }
       }
-    return false;
+    return false;//otherwise return false
   }
 
   /**
@@ -74,10 +73,10 @@ public class InputHandler {
   public void process(char k) {
      //@ assert false;
    if(legal_input(k) == true){
-    if(k == DISPLAY_HIGH_SCORES){
+    if(k == DISPLAY_SCORES){
       //display high scores
     }
-    else if (k == TOGGLE_MUSIC_OR_SOUND_EFFECTS){
+    else if (k == TOGGLE_MUSIC){
       //toggle music or sound effects
       
     }
