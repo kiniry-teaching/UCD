e21e203d8ddc81ddea4f20ac4a10fa635dcb5663
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-07 18:54:22 +0000

    Javadocced and stylechecked AbstractPhysics

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/AbstractPhysics.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/AbstractPhysics.java
@@ -1,43 +1,55 @@
 package thrust.physics;
 
+/**
+ * Implementation of Physics methods.
+ * @author Colin Casey (colin.casey.2@gmail.com)
+ * @version 7 April 2008
+ */
 public abstract class AbstractPhysics implements Physics {
-  
-  final double G = -9.8;
-  double mass;
-  double speed;
-  double rate_of_speed;
-  double orientation;
-  double x;
-  double y;
+  /** The force that attracts the spaceship and
+   * goal sphere toward the terrain. */
+  final double my_gravity_constant = -9.8;
+  /** The quantity of matter that an entity contains. */
+  double my_mass;
+  /** The rate of motion. */
+  double my_speed;
+  /** Change in the speed of something. */
+  double my_rate_of_speed;
+  /** The relative physical direction of entities. */
+  double my_orientation;
+  /** The x co-ordinate where an entity is located. */
+  double my_position_x;
+  /** The y co-ordinate where an entity is located. */
+  double my_position_y;
 
   public double[] acceleration() {
-    double[] acceleration = {speed, rate_of_speed};
+    final double[] acceleration = {my_speed, my_rate_of_speed};
     return acceleration;
   }
 
   public double gravitational_constant() {
-    return G;
+    return my_gravity_constant;
   }
 
   public double mass() {
-    return mass;
+    return my_mass;
   }
 
   public double momentum() {
-    return mass*speed;
+    return my_mass * my_speed;
   }
 
   public double orientation() {
-    return orientation;
+    return my_orientation;
   }
 
   public double[] position() {
-    double[] position = {x, y};
+    final double[] position = {my_position_x, my_position_y};
     return position;
   }
 
   public double[] velocity() {
-    double[] velocity = {speed, orientation};
+    final double[] velocity = {my_speed, my_orientation};
     return velocity;
   }
 
