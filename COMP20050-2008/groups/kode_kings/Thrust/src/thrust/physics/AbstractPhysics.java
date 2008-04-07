009f92913c8b9c202832dac6ad75c5eb6581d2ce
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-07 19:34:19 +0000

    Added more javadocs to AbstractPhysics

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/AbstractPhysics.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/AbstractPhysics.java
@@ -22,35 +22,55 @@ public abstract class AbstractPhysics implements Physics {
   /** The y co-ordinate where an entity is located. */
   double my_position_y;
 
+  /**
+   * @return What is your acceleration in meters per second squared?
+   */
   public double[] acceleration() {
     final double[] acceleration = {my_speed, my_rate_of_speed};
     return acceleration;
   }
 
+  /**
+   * @return What is the gravitational constant?
+   */
   public double gravitational_constant() {
     return my_gravity_constant;
   }
 
+  /**
+   * @return What is your mass in kilogrammes?
+   */
   public double mass() {
     return my_mass;
   }
 
+  /**
+   * @return What is your momentum in kilogrammes*meters per second?
+   */
   public double momentum() {
     return my_mass * my_speed;
   }
 
+  /**
+   * @return What is your orientation in radians?
+   */
   public double orientation() {
     return my_orientation;
   }
 
+  /**
+   * @return What is your position in meters from the origin?
+   */
   public double[] position() {
     final double[] position = {my_position_x, my_position_y};
     return position;
   }
 
+  /**
+   * @return What is your velocity in meters per second?
+   */
   public double[] velocity() {
     final double[] velocity = {my_speed, my_orientation};
     return velocity;
   }
-
 }
