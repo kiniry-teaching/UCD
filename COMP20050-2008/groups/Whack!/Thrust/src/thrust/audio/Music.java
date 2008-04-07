144d4d72f9f19d36a339c11dcb3be4bb678ecea4
06584551 <06584551@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-07 21:32:31 +0000

    Music File

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/audio/Music.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/audio/Music.java
@@ -5,31 +5,70 @@ package thrust.audio;
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 2 April 2008
  */
-public class Music {
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+/**
+ * 
+ * @author allison
+ *
+ */
+public class Music extends Exception {
   //@ public model boolean is_playing;
 
   /**
    * @return Is music playing?
    */
+  private static final long serialVersionUID = 1L;
   //@ ensures \result == is_playing;
-  public /*@ pure @*/ boolean playing() {
-    assert false; //@ assert false;
-    return false;
+  /**
+   * 
+   */
+  private Clip music;
+
+  /**
+   * 
+   */
+  public Music(){
+    try{
+      File musicFile = new File("/media/Thrustmusic.wav");
+      AudioInputStream mus = AudioSystem.getAudioInputStream(musicFile);
+
+      // load the sound into memory (a Clip)
+      DataLine.Info info = new DataLine.Info(Clip.class, mus.getFormat());
+      Clip clip = (Clip) AudioSystem.getLine(info);
+      clip.open(mus);
+
+    }catch(Exception e){
+      System.out.println("Error");
+    }
+
+  }
+  /**
+   * 
+   * @return
+   */
+  public final  /*@ pure @*/ boolean playing() {
+    return music.isRunning();
+    //@ ensures is_playing;
   }
 
   /**
    * Start playing the music.
    */
-  //@ ensures is_playing;
-  public void start() {
-    assert false; //@ assert false;
+
+  public final void start() {
+    music.loop(Clip.LOOP_CONTINUOUSLY);
   }
 
   /**
    * Stop playing the music.
    */
   //@ ensures !is_playing;
-  public void stop() {
-    assert false; //@ assert false;
+  public final void stop() {
+    music.stop();
   }
 }
