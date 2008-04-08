77b0da2480eea81fc36624e16e2890cf1bcd3af5
dominiccarr <dominiccarr@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-08 13:47:19 +0000

    Changed EntityPhysics

--- COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/physics/EntityPhysics.java
+++ COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/physics/EntityPhysics.java
@@ -10,6 +10,35 @@ public class EntityPhysics implements Physics {
    * The gravitational constant.
    */
   private final double my_grav_constant;
+  /**
+   * The Mass.
+   */
+  private final double my_mass;
+  /**
+   * The speed.
+   */
+  private final double my_speed;
+  /**
+   * the orientation.
+   */
+  private final double my_orientation;
+  /**
+   * the weight.
+   */
+  private final double my_weight_kilograms;
+  /**
+   * the velocity.
+   */
+  private final double[] my_velocity;
+  /**
+   * the position.
+   */
+  private final double[] my_position;
+  /**
+   * the accelaration.
+   */
+  private final double[] my_acceleration;
+  
 
   //@ constraint (* The gravitational constant never changes. *);
   //@ constraint gravitational_constant() == \old(gravitational_constant());
@@ -19,6 +48,7 @@ public class EntityPhysics implements Physics {
    */
   //@ ensures \result.length == 2;
   public/*@ pure @*/double[] acceleration() {
+    return my_acceleration;
   }
 
   /**
@@ -33,18 +63,21 @@ public class EntityPhysics implements Physics {
    */
   //@ ensures 0 <= \result;
   public/*@ pure @*/double mass() {
+    return my_mass;
   }
 
   /**
    * @return What is your momentum in kilograms*meters per second?
    */
   public/*@ pure @*/double momentum() {
+    return my_speed * my_weight_kilograms;
   }
 
   /**
    * @return What is your orientation in radians?
    */
   public/*@ pure @*/double orientation() {
+    return my_orientation;
   }
 
   /**
@@ -52,11 +85,13 @@ public class EntityPhysics implements Physics {
    */
   //@ ensures \result.length == 2;
   public/*@ pure @*/double[] position() {
+    return my_position;
   }
 
   /**
    * @return What is your velocity in meters per second?
    */
   public/*@ pure @*/double[] velocity() {
+    return my_velocity;
   }
 }
