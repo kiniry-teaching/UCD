28c1ae81d5032686b7fba023c9f2141a807975d1
ciaranhale <ciaranhale@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-08 12:49:27 +0000

    Another attempt audio

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/audio/Music.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/audio/Music.java
@@ -1,9 +1,18 @@
 
 package thrust.audio;
 
-import  sun.audio.*;    //import the sun.audio package
-import  java.io.*;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioFormat;
+   import javax.sound.sampled.AudioInputStream;
+  import javax.sound.sampled.AudioSystem;
+   import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+ 
 
 /**
  * In-game music.
@@ -12,10 +21,37 @@ import  java.io.*;
  */
 public class Music {
   //@ public model boolean is_playing;
+  private final transient File clipFile = new File("");
+ 
 
-  FileInputStream myMusicFile = new FileInputStream();
-  AudioStream mySong = new AudioStream(myMusicFile);
-  /**
+    private transient Clip myclip;
+
+    
+    public Music() throws Exception, Exception {
+            AudioInputStream myaudioInputStream = null;
+ 
+            final DataLine.Info info = new DataLine.Info(SourceDataLine.class, myaudioInputStream.getFormat());
+            
+            try {
+              myaudioInputStream = AudioSystem.getAudioInputStream(clipFile);
+            } catch (UnsupportedAudioFileException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            } catch (IOException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            }         
+            
+            try {
+              myclip = (Clip) AudioSystem.getLine(info);
+            } catch (LineUnavailableException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            }
+                myclip.open(myaudioInputStream);
+
+    }
+            /**
    * 
    * @return Is music playing?
    */
@@ -31,8 +67,7 @@ public class Music {
   //@ ensures is_playing;
   public void start() {
     
-    AudioPlayer.player.start(mySong); 
-    
+    myclip.loop(Clip.LOOP_CONTINUOUSLY);
     assert false; //@ assert false;
   }
 
@@ -42,7 +77,8 @@ public class Music {
   //@ ensures !is_playing;
   public void stop() {
    
-    AudioPlayer.player.stop(mySong);
+    myclip.stop();
+          
     assert false; //@ assert false;
   }
 }
