4ee2dd2752e462d414f4512f4af68da3852c379c
mewtio <mewtio@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-08 16:27:12 +0000

    ursula added physics

--- COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/physics/Physics.java
+++ COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/physics/Physics.java
@@ -1,50 +1,74 @@
 package thrust.physics;
 
 /**
- * Computing the behavior of entities according to physical
+ * Implementing the behavior of entities according to physical
  * simulation in two dimensions.
- * @author Joe Kiniry (kiniry@acm.org)
- * @version 2 April 2008
+ * @author Ursula Redmond (ursula.redmond@ucdconnect.ie)
+ * @version 8 April 2008
  */
-public interface Physics {
-  //@ constraint (* The gravitational constant never changes. *);
+public class Physics {
+//@ constraint (* The gravitational constant never changes. *);
   //@ constraint gravitational_constant() == \old(gravitational_constant());
 
   /**
    * @return What is your acceleration in meters per second squared?
    */
   //@ ensures \result.length == 2;
-  /*@ pure @*/ double[] acceleration();
+  /*@ pure @*/ double[] acceleration()
+  {
+    return null;
+  }
 
   /**
    * @return What is the gravitational constant?
    */
-  /*@ pure @*/ double gravitational_constant();
+  /*@ pure @*/ double gravitational_constant()
+  {
+    final double gravity = 9.8;
+    return gravity;
+  }
 
   /**
    * @return What is your mass in kilograms?
    */
   //@ ensures 0 <= \result;
-  /*@ pure @*/ double mass();
+  /*@ pure @*/ double mass()
+  {
+    final double mass = 50000;
+    return mass;
+  }
 
   /**
    * @return What is your momentum in kilograms*meters per second?
    */
-  /*@ pure @*/ double momentum();
+  /*@ pure @*/ double momentum()
+  {
+    return 0;
+  }
 
   /**
    * @return What is your orientation in radians?
    */
-  /*@ pure @*/ double orientation();
+  /*@ pure @*/ double orientation()
+  {
+    return 0;
+  }
 
   /**
    * @return What is your position in meters from the origin?
    */
   //@ ensures \result.length == 2;
-  /*@ pure @*/ double[] position();
+  /*@ pure @*/ double[] position()
+  {
+    return null;
+  }
 
   /**
    * @return What is your velocity in meters per second?
    */
-  /*@ pure @*/ double[] velocity();
+  /*@ pure @*/ double[] velocity()
+  {
+    return null;
+  }
+
 }
