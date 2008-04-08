1bd7ba3336605656c7e57637e7fc49488fb57ebd
06584551 <06584551@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-08 15:08:28 +0000

    Fixed Errors

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/audio/SoundEffect.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/audio/SoundEffect.java
@@ -1,51 +1,71 @@
 package thrust.audio;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 /**
  * An example of loading and playing a sound using a Clip. This complete class
  * isn't in the book ;)
+ * @author Joe Kiniry (kiniry@acm.org)
+ * @version 2 April 2008
  */
 public class SoundEffect extends Exception {
-/**
- *
- */
+  /**
+   *
+   */
   private static final long serialVersionUID = 1L;
   /**
-   * 
+   *
    */
-  private Clip bit;
-  
-   /**
+  private Clip my_bit;
+
+  /**
    *@return SoundEffect.;
-   *@param soundfile inputs a sound.; 
+   *@param soundfile inputs a sound.;
    */
-  public final/*@pure@*/ SoundEffect  make(final File soundfile) {
+  public final/*@pure@*/ SoundEffect  make(final File the_soundfile) {
     // specify the sound to play
     // (assuming the sound can be played by the audio system)
-    
-     
-    
-    try {
-      File soundFile = new File("/.../media/Thrust.wav");
-      AudioInputStream effect = AudioSystem.getAudioInputStream(soundFile);
 
-      // load the sound into memory (a Clip)
-      DataLine.Info info = new DataLine.Info(Clip.class, effect.getFormat());
-      Clip clip = (Clip) AudioSystem.getLine(info);
-      clip.open(effect);
-      return null;
-    } catch (Exception e) {
-      System.out.println("Error");
-    }
-      
-      while (true) {
-        bit.loop(Clip.LOOP_CONTINUOUSLY);
+    final File soundFile = new File("/.../media/Thrust.wav");
+
+    try {
+      final AudioInputStream effect;
+      effect = AudioSystem.getAudioInputStream(soundFile);
+      final DataLine.Info info;
+      info = new DataLine.Info(Clip.class, effect.getFormat());
+      Clip clip;
+      try {
+        clip = (Clip) AudioSystem.getLine(info);
+        try {
+          clip.open(effect);
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-      
+      } catch (LineUnavailableException e) {
+
+        e.printStackTrace();
+      }
+    } catch (UnsupportedAudioFileException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    final SoundEffect sou = (SoundEffect) my_bit;
+    return sou;
+    // load the sound into memory (a Clip)
   }
-}        
+  public final void start() {
+    my_bit.loop(Clip.LOOP_CONTINUOUSLY);
+
+
+  }
+}
