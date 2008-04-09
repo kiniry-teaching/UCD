fe701c6323fff6f36fc391dedc164c15ab65bb1d
ne0dym1um <ne0dym1um@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-09 11:37:24 +0000

    new parser.h

--- COMP30050-2008/groups/kinephon/source/Parser/Parser.h
+++ COMP30050-2008/groups/kinephon/source/Parser/Parser.h
@@ -1,24 +1,30 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
-#include "../rai/Recorder/Recorder.h"
+//#include "IParserRecorder.h"
 #include <string>
 #include <iostream>
 #include <stdlib.h>
+#include <cmath>
 using namespace std;
 
 class Parser
 {
 public:
 	Parser();
-	Parser(*Recorder);
+//	Parser(IParserRecorder);
 	void supply(string in){parser(in, in.size());};
 	virtual ~Parser();
 private:
 	void parser(string,int);
 	void provide();
-	int array[12];
+	void binary(int);
+	int MSB_Extract(int,int);
+	int Size_Extract();
+	int reportData[12];
+	int MSB_Size_Array[8];
 	int control[4];
+	int binaryPosition;
 };
 
 #endif /*PARSER_H_*/
