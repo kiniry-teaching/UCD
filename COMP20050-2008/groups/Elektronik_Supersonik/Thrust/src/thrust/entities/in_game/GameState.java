07cbeddae4dbfcdd2aa8690f4112ebfbc110dc7f
dominiccarr <dominiccarr@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-11 12:51:11 +0000

    Added GameState

---
+++ COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/entities/in_game/GameState.java
@@ -0,0 +1,66 @@
+package thrust.entities.in_game;
+
+/**
+ * @author Dominic Carr (dominiccarr@gmail.com).
+ * @version 11 April 2008
+ */
+public class GameState extends AbstractGameState {
+
+  public void add_high_score(final HighScoreInterface the_new_high_score) {
+    // TODO Auto-generated method stub
+  }
+
+  public int bonus() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void change_lives(final byte some_new_lives) {
+    // TODO Auto-generated method stub
+  }
+
+  public void change_score(final int some_new_points) {
+    // TODO Auto-generated method stub
+  }
+
+  public int current_fuel() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public HighScoreInterface high_score(final int the_index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public HighScoreInterface[] high_scores() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public byte lives() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public int maximum_fuel() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void new_bonus(final int the_new_value) {
+    // TODO Auto-generated method stub
+  }
+
+  public boolean new_high_score(
+                                final HighScoreInterface the_possible_new_high_score) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public int score() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
