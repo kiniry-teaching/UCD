ccaa93ecb78d81ab0a351aa763b3c1324cc574c5
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-12 20:06:21 +0000

    More merged stuff

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/PhysicsInterface.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/PhysicsInterface.java
@@ -1,12 +1,23 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
 package thrust.physics;
 
 /**
- * Computing the behaviour of entities according to physical
+ * Computing the behavior of entities according to physical
  * simulation in two dimensions.
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 2 April 2008
  */
-public interface Physics {
+public interface PhysicsInterface {
+  //@ constraint (* The gravitational constant never changes. *);
   //@ constraint gravitational_constant() == \old(gravitational_constant());
 
   /**
@@ -21,13 +32,13 @@ public interface Physics {
   /*@ pure @*/ double gravitational_constant();
 
   /**
-   * @return What is your mass in kilogrammes?
+   * @return What is your mass in kilograms?
    */
   //@ ensures 0 <= \result;
   /*@ pure @*/ double mass();
 
   /**
-   * @return What is your momentum in kilogrammes*meters per second?
+   * @return What is your momentum in kilograms*meters per second?
    */
   /*@ pure @*/ double momentum();
 
