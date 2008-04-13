aaef7662a19f717f55955eb32f81e0f99241b56c
steemcb <steemcb@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-13 16:30:57 +0000

    rejigged input handler

--- COMP20050-2008/groups/Thick,_Dapper_Deviants/Thrust/src/thrust/input/InputHandler.java
+++ COMP20050-2008/groups/Thick,_Dapper_Deviants/Thrust/src/thrust/input/InputHandler.java
@@ -13,43 +13,50 @@ public class InputHandler {
   /** An unknown character code. */
   //private static final char UNKNOWN_CHAR = '\0';
   /** Fill in this comment. */
-	//public boolean my_keyDown (final Event a_e, final int an_key) {
-	//	
-	//}
-  public static final char DISPLAY_HIGH_SCORES = 'h';
+  //public boolean my_keyDown (final Event a_e, final int an_key) {
+  //  
+  //}
+  
+  
+  
+  
   /** Calls high score screen. */
+  public static final char DISPLAY_HIGH_SCORES = 'h';
+  /** Sets music and effects to false or true. */
   public static final char TOGGLE_MUSIC_OR_EFFECTS = 'm';
-  /** Sets music playing and effects to false. */
-  public static final char START_GAME = (char) 13;
   /** Started by Enter. */
-  public static final char STOP_GAME = (char) 27;
+  public static final char START_GAME = (char) 13;
   /** Halts game using Esc key. */
-  public static final char FIRE_GUN = (char) 32;
+  public static final char STOP_GAME = (char) 27;
   /** Spacebar. */
-  public static final char TURN_LEFT = 'a';
+  public static final char FIRE_GUN = (char) 32;
   /** hit a to turn left. */
-  public static final char TURN_RIGHT = 'd';
+  public static final char TURN_LEFT = 'a';
   /** hit d to turn right. */
-  public static final char USE_ENGINE = 'w';
+  public static final char TURN_RIGHT = 'd';
   /** hit w to thrust. */
-  public static final char USE_SHIELD = 'k';
+  public static final char USE_ENGINE = 'w';
   /** hit i to use shield. */
+  public static final char USE_SHIELD = 'k';
+  
   /**
    * @return What are the legal keyboard inputs?
    */
-  public static char legal_inputs[]=new char [9]; {
+  public char[] legal_inputs() {
     //assert false; //@ assert false;
     //return null;
-	legal_inputs[0] = DISPLAY_HIGH_SCORES;
-	legal_inputs[1] = TOGGLE_MUSIC_OR_EFFECTS;
-	legal_inputs[2] = START_GAME;
-	legal_inputs[3] = STOP_GAME;
-	legal_inputs[4] = FIRE_GUN;
-	legal_inputs[5] = TURN_LEFT;
-	legal_inputs[6] = TURN_RIGHT;
-	legal_inputs[7] = USE_ENGINE;
-	legal_inputs[8] = USE_SHIELD;
-	
+  final char legal_inputs[] = new char[9];
+  legal_inputs[0] = DISPLAY_HIGH_SCORES;
+  legal_inputs[1] = TOGGLE_MUSIC_OR_EFFECTS;
+  legal_inputs[2] = START_GAME;
+  legal_inputs[3] = STOP_GAME;
+  legal_inputs[4] = FIRE_GUN;
+  legal_inputs[5] = TURN_LEFT;
+  legal_inputs[6] = TURN_RIGHT;
+  legal_inputs[7] = USE_ENGINE;
+  legal_inputs[8] = USE_SHIELD;
+  assert legal_inputs != null;
+  return legal_inputs;
   }
 
   /**
@@ -66,22 +73,26 @@ public class InputHandler {
     @                      (the_character == USE_ENGINE) |
     @                      (the_character == USE_SHIELD);
     @*/
- public char keyCode;
-  public char keyPressed(KeyEvent e){
-     keyCode = (char) e.getKeyCode();
-     return keyCode;
-  }
+ //final char keyCode;
   
-  public /*@ pure @*/ boolean legal_input(){
-      
+  
+ 
+  public /*@ pure @*/ boolean legal_input(final char the_character) {
+    final char[] legal_inputs = legal_inputs();
+    assert (legal_inputs != null);
     
-    for(int i=0; i<9; i++){
-        if (legal_inputs[i] == keyCode){
+    for(int i=0; i<legal_inputs.length; i++){
+        if (legal_inputs[i] == the_character){
           return true;
         }
       }
     return false;
   } 
+  
+  public char keyPressed(KeyEvent e){
+    char keyCode = (char) e.getKeyCode();
+    return keyCode;
+ }
 
   /**
    * Process this keyboard input character.
@@ -89,6 +100,47 @@ public class InputHandler {
    */
   //@ requires legal_input(the_keyboard_input);
   public void process(char the_keyboard_input) {
-    assert false; //@ assert false;
+    //assert false; //@ assert false;
+    /** Calls high score screen. */
+    if(the_keyboard_input == DISPLAY_HIGH_SCORES){
+      assert true;
+    }
+    /** Sets music and effects to false or true. */
+    else if (the_keyboard_input == TOGGLE_MUSIC_OR_EFFECTS){
+      assert true;
+    }
+    /** Starts the game. */
+    else if (the_keyboard_input == START_GAME){
+      assert true;
+    }
+    /** Ends the game. */
+    else if (the_keyboard_input == STOP_GAME){
+      
+    }
+    /** Fires the gun. */
+    else if(the_keyboard_input == FIRE_GUN){
+      assert true;
+    }
+    /** Turn spaceship left. */
+    else if(the_keyboard_input == TURN_LEFT){
+      assert true;
+    }
+    /** Turn spaceship right. */
+    else if(the_keyboard_input == TURN_RIGHT){
+      assert true;
+    }
+    /** Engages engine. */
+    else if(the_keyboard_input == USE_ENGINE){
+      assert true;
+    }
+    /** Invokes shield. */
+  else if(the_keyboard_input == USE_SHIELD){
+    assert true;
+  }
+    /** Not a valid command. */
+  else{
+    assert false;
+  }
   }
 }
+
