fc9b2032d66d9214b7f35c1436a56b831d2f717d
mewtio <mewtio@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-14 15:13:03 +0000

    <3 = :(

--- COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/entities/in_game/AbstractGameState.java
+++ COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/entities/in_game/AbstractGameState.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package thrust.entities.in_game;
 
@@ -28,7 +28,7 @@ public abstract /*@ pure @*/ class AbstractGameState {
 
   /**
    * @return How much fuel do you contain?
-   * @design The current fuel of the spaceship
+   * @make The current fuel of the spaceship
    * @note Note that the {@link Spaceship} class should be the
    * actual owner of this data; this is just a convenience method.
    */
@@ -37,7 +37,7 @@ public abstract /*@ pure @*/ class AbstractGameState {
 
   /**
    * @return How much fuel can you contain?
-   * @design The maximum fuel of the spaceship.
+   * @ The maximum fuel of the spaceship.
    */
   //@ ensures 0 <= \result;
   public abstract /*@ pure @*/ int maximum_fuel();
