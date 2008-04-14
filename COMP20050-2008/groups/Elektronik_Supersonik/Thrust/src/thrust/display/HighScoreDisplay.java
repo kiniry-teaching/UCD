5d4bfbf17c7c31802079ef053b004e3e40abfed7
dominiccarr <dominiccarr@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-14 11:20:23 +0000

    Added InfoPanel and HighScoreDisplay

---
+++ COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/display/HighScoreDisplay.java
@@ -0,0 +1,25 @@
+package thrust.display;
+
+public class HighScoreDisplay extends AbstractHighScoreDisplay{
+
+  public void add_new_high_score() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void display() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public boolean displayed() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public void hide() {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
---
+++ COMP20050-2008/groups/Elektronik_Supersonik/Thrust/src/thrust/display/InfoPanel.java
@@ -0,0 +1,21 @@
+package thrust.display;
+
+public class InfoPanel extends AbstractInfoPanel{
+
+  public void display() {
+    // TODO Auto-generated method stub
+  }
+
+  public boolean displayed() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public void hide() {
+    // TODO Auto-generated method stub
+  }
+
+  public void update() {
+    // TODO Auto-generated method stub
+  }
+}
