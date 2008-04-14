8ff97c7ffe94f05b4c4cbbd5d1c577edf61b4d75
david <david@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-14 14:47:12 +0000

    FuelableWhack class

---
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/about/FuelableWhack.java
@@ -0,0 +1,70 @@
+package thrust.entities.about;
+/**
+ * @author David Maguire (David.Maguire.2@ucdconnect.ie)
+ * @version 11 April 2008
+ */
+public class FuelableWhack implements Fuelable {
+
+  /**
+   * the fuel.
+   */
+  int my_fuel;
+  /**
+   * @return How much fuel do you contain?
+   */
+  //@ ensures 0 <= \result;
+  //@ ensures \result <= maximum_fuel();
+  public /*@ pure @*/ int fuel() {
+    if (my_fuel > maximum_fuel()) {
+      System.out.print("Cannot exceed max fuel");
+    }
+
+    return my_fuel;
+  }
+
+  /**
+   * @return How much fuel can you contain?
+   */
+  //@ ensures 0 <= \result;
+  public /*@ pure @*/ int maximum_fuel() {
+    final int maxFuel = 9999;
+    return maxFuel;
+  }
+
+  /**
+   * @param the_fuel_content This many units is your fuel content.
+   */
+  //@ requires 0 <= the_fuel_content & the_fuel_content <= maximum_fuel();
+  //@ ensures fuel() == the_fuel_content;
+  public void set_fuel_content(final int the_fuel_content) {
+    if (the_fuel_content >= 0 && the_fuel_content <= maximum_fuel()) {
+      my_fuel = the_fuel_content;
+    }
+  }
+  /**
+   * @param the_fuel_change Change your fuel content by this many units.
+   */
+  /*@ ensures (\old(fuel() + the_fuel_change < 0) ?
+      @            (fuel() == 0) :
+      @          (\old(maximum_fuel() < (fuel() + the_fuel_change)) ?
+      @             (fuel() == maximum_fuel()) :
+      @           fuel() == \old(fuel() + the_fuel_change)));
+      @*/
+  public void change_fuel_content(final int the_fuel_change) {
+
+    if (my_fuel + the_fuel_change < 0) {
+      my_fuel = 0;
+    }
+
+    if (my_fuel + the_fuel_change > maximum_fuel()) {
+      my_fuel = maximum_fuel();
+    }
+
+    my_fuel = my_fuel + the_fuel_change;
+
+    //@ invariant (* Fuel content is always non-negative and finite. *);
+    //@ invariant 0 <= fuel();
+  }
+}
+
+
