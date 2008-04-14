0fdcb80485fb2004a63b1a87bfdef1a9bf5bab52
mewtio <mewtio@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-14 15:05:23 +0000

    >< life sucks at times

--- COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/entities/about/Fuelable.java
+++ COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/entities/about/Fuelable.java
@@ -14,26 +14,49 @@ package thrust.entities.about;
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 11 April 2008
  */
-public interface Fuelable {
+
+
+
+public class Fuelable {
   /**
    * @return How much fuel do you contain?
    */
   //@ ensures 0 <= \result;
   //@ ensures \result <= maximum_fuel();
-  /*@ pure @*/ int fuel();
+  /*@ pure @*/
+
+  int my_fuel;
+  int fuel()
+  {
+
+     my_fuel = maximum_fuel();
+    return my_fuel;
+
+
+  }
 
   /**
    * @return How much fuel can you contain?
    */
   //@ ensures 0 <= \result;
-  /*@ pure @*/ int maximum_fuel();
-
+  /*@ pure @*/ int maximum_fuel()
+  {
+    final int maximum_fuel = 9999;
+    return maximum_fuel;
+  }
   /**
    * @param the_fuel_content This many units is your fuel content.
    */
   //@ requires 0 <= the_fuel_content & the_fuel_content <= maximum_fuel();
   //@ ensures fuel() == the_fuel_content;
-  void set_fuel_content(int the_fuel_content);
+  void set_fuel_content(int the_fuel_content)
+  {
+    if (the_fuel_content > 0)
+    {
+      my_fuel = the_fuel_content;
+    }
+
+  }
 
   /**
    * @param the_fuel_change Change your fuel content by this many units.
@@ -44,7 +67,21 @@ public interface Fuelable {
     @             (fuel() == maximum_fuel()) :
     @           fuel() == \old(fuel() + the_fuel_change)));
     @*/
-  void change_fuel_content(int the_fuel_change);
+  void change_fuel_content(final int the_fuel_change)
+  {
+    if (my_fuel + the_fuel_change < 0)
+    {
+      my_fuel = 0;
+    }
+
+    if (my_fuel + the_fuel_change > maximum_fuel())
+    {
+      my_fuel = maximum_fuel();
+    }
+
+    my_fuel = my_fuel + the_fuel_change;
+
+  }
 
   //@ invariant (* Fuel content is always non-negative and finite. *);
   //@ invariant 0 <= fuel();
