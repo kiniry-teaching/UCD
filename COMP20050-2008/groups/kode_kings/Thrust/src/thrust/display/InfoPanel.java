bb9f29047a41a7877f1231d2b3f5d258605b0f2f
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-15 13:09:08 +0000

    Created InfoPanel

---
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/display/InfoPanel.java
@@ -0,0 +1,19 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Colin Casey (colin.casey@org.com)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "April 2008"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
+package thrust.display;
+
+/**
+ * @author Colin Casey (colin.casey@org.com)
+ * @version 15 April 2008
+ */
+public class InfoPanel extends AbstractInfoPanel {
+
+}
