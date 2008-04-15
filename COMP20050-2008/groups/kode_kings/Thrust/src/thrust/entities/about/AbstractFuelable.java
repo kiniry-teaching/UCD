bd18c8e1951003f7fef5e99acff3e9f03d57717f
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-15 12:33:49 +0000

    Created AbstractFuelable

---
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/about/AbstractFuelable.java
@@ -0,0 +1,20 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Colin Casey (colin.casey.2@gmail.com)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "April 2008"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
+package thrust.entities.about;
+
+/**
+ * @author Colin Casey (colin.casey.2@gmail.com)
+ * @version 15 April 2008
+ */
+public abstract class AbstractFuelable {
+
+
+}
