e184eaceaad68c9afad30d9efe8155a306a3209f
ciaranhale <ciaranhale@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-15 13:36:45 +0000

    Created AbstractHighScore

---
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/about/AbstractHighScore.java
@@ -0,0 +1,32 @@
+package thrust.entities.about;
+
+import thrust.entities.about.AbstractGameState.HighScoreInterface;
+
+/**
+ * @author Ciaran Hale (ciaran.hale@ucd.ie)
+ * @version 15 April 2008
+ */
+
+public class AbstractHighScore implements HighScoreInterface {
+
+  public char[] initials() {
+
+    return initials();
+  }
+
+  public void new_initials(final char[] the_new_initials) {
+
+
+  }
+
+  public void new_score(final int the_new_score) {
+
+
+  }
+
+  public int score() {
+
+    return score();
+  }
+
+}
