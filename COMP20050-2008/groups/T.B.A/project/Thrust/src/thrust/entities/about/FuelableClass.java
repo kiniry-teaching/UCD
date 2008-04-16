3986867b10956bda84a2e87eeeddbab7dd44469f
jdouglas <jdouglas@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-16 16:11:48 +0000

    Fixes

--- COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/entities/about/FuelableClass.java
+++ COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/entities/about/FuelableClass.java
@@ -1,6 +1,14 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
 package thrust.entities.about;
-
-
 /**
  * Abstract class implemented by the Fuelable interface.
  * @author DH, JD and EH (jd2088@gmail.com)
@@ -11,34 +19,34 @@ public abstract class FuelableClass implements Fuelable {
 /**
 * The amount of fuel that is contained.
 */
-private int my_fuel;
+  private int my_fuel;
 /**
 * The change in the fuel content by a specific amount of units.
 */
-private int my_fuel_content;
+  private int my_fuel_content;
 //@ ensures 0 <= \result;
 //@ ensures \result <= maximum_fuel();
 /*@ pure @*/
-public int fuel() {
-return my_fuel; 
-}
+  public int fuel() {
+    return my_fuel;
+  }
 /**
  * returns the maximum amount of fuel that can you contain?
  */
 //@ ensures 0 <= \result;
 /*@ pure @*/
-public int maximum_fuel() {
-  final int an_max_fuel = 9999;
-  return an_max_fuel;
-}
+  public int maximum_fuel() {
+    final int an_max_fuel = 9999;
+    return an_max_fuel;
+  }
 /**
  * @param the_fuel_content This many units is your fuel content.
  */
 //@ requires 0 <= the_fuel_content & the_fuel_content <= maximum_fuel();
 //@ ensures fuel() == the_fuel_content;
-public void set_fuel_content(int the_fuel_content) {
-  my_fuel = the_fuel_content;
-}
+  public void set_fuel_content(final int the_fuel_content) {
+    my_fuel = the_fuel_content;
+  }
 /**
  * @param the_fuel_change Change your fuel content by this many units.
  */
@@ -50,7 +58,7 @@ public void set_fuel_content(int the_fuel_content) {
 @*/
 //@ invariant (* Fuel content is always non-negative and finite. *);
 //@ invariant 0 <= fuel()
-public void change_fuel_content(int the_fuel_change) {
-  my_fuel_content = the_fuel_change;
- }
+  public void change_fuel_content(final int the_fuel_change) {
+    my_fuel_content = the_fuel_change;
+  }
 }
