85530f8fac0f9508efc4a78f0d142ee871e5417f
uredmond <uredmond@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 12:38:03 +0000

    Moo

--- COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/audio/SoundEffect.java
+++ COMP20050-2008/groups/We_Are_Scientists/Thrust/src/thrust/audio/SoundEffect.java
@@ -1,15 +1,16 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
 package thrust.audio;
 
 import java.io.File;
-import java.io.IOException;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-
 
 /**
  * Any sound made in response to a event.
@@ -22,37 +23,15 @@ public class SoundEffect {
    * @param the_sound_effect_file the sound effect to make.
    * @return the new sound effect for the effect stored in 's'.
    */
-  //public static void main(String[] args) throws Exception {
-  private  Clip my_clip;
-  /**
-   * Any sound made in response to a event.
-   * @author simon
-   * @param  soundFile sound file
-   */
-  public final/* @ pure @ */SoundEffect make(final File the_sound) throws
-  IOException ,
-  UnsupportedAudioFileException ,
-  LineUnavailableException {
-
-    AudioInputStream sound = null;
-
-
-    sound = AudioSystem.getAudioInputStream(the_sound);
-
-          //final AudioFormat format = audioInputStream.getFormat();
-    final DataLine.Info info =
-            new DataLine.Info(Clip.class, sound.getFormat());
-    my_clip = (Clip) AudioSystem.getLine(info);
-    my_clip.open(sound);
+  public static /*@ pure @*/ SoundEffect make(File the_sound_effect_file) {
+    assert false; //@ assert false;
     return null;
   }
-
 
   /**
    * Start playing your effect.
    */
-
-  public final void start() {
-    my_clip.start();
+  public void start() {
+    assert false; //@ assert false;
   }
 }
