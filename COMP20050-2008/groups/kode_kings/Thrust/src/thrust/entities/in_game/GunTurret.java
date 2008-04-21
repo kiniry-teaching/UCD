7ffd09199631fbe323f1bf737a2117798597b168
ciaranhale <ciaranhale@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 16:17:23 +0000

    unimplemented methods

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/GunTurret.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/GunTurret.java
@@ -9,8 +9,12 @@
  */
 package thrust.entities.in_game;
 
+import java.awt.Color;
+import java.awt.Shape;
+
 import thrust.entities.EnemyEntity;
 import thrust.entities.StaticEntity;
+import thrust.entities.behaviors.AI;
 
 /**
  * An enemy gun turret that shoots bullets at the spaceship.
@@ -19,6 +23,106 @@ import thrust.entities.StaticEntity;
  */
 public class GunTurret extends StaticEntity
   implements EnemyEntity {
+
+  public double[] acceleration() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double mass() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double momentum() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] velocity() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void render() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Shape shape() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void shape(Shape the_shape) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public String shape_name() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public byte state() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void state(byte the_state) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public AI attack() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void attack(AI the_behavior) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public AI disturb() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void disturb(AI the_behavior) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public double gravitational_constant() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double orientation() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] position() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void simulate(double some_seconds) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Color color() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void color(Color the_color) {
+    // TODO Auto-generated method stub
+    
+  }
   /*@ public invariant (* A gun turret always resides on/adjacent to
     @                     the terrain. *);
     @ public invariant (* A gun turret's color is always green. *);
