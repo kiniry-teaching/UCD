cb8ac5584c0957d0b6856d322e9a56fc22639a80
ciaranhale <ciaranhale@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 15:58:00 +0000

    Override implement methods

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/FuelPod.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/FuelPod.java
@@ -9,6 +9,9 @@
  */
 package thrust.entities.in_game;
 
+import java.awt.Color;
+import java.awt.Shape;
+
 import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
 import thrust.entities.about.Fuelable;
@@ -20,6 +23,111 @@ import thrust.entities.about.Fuelable;
  */
 public class FuelPod extends StaticEntity
   implements NeutralEntity, Fuelable {
+
+  public double gravitational_constant() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double orientation() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] position() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void simulate(double some_seconds) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void change_fuel_content(int the_fuel_change) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public int fuel_mass() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public int fuel() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public int maximum_fuel() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void set_fuel_content(int the_fuel_content) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public double[] acceleration() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double mass() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double momentum() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] velocity() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void render() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public String shape_name() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Shape shape() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void shape(Shape the_shape) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public byte state() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void state(byte the_state) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Color color() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void color(Color the_color) {
+    // TODO Auto-generated method stub
+    
+  }
   /*@ public invariant (* A fuel pod is destroyed by a bullet. *);
     @ public invariant (* The fuel pod is not affected by the goal sphere. *);
     @ public invariant (* The fuel pod is not affected by the spaceship. *);
