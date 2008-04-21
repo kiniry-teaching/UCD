3e3bd162abea2280a3adbd23db94ec432d47259a
Tara <Tara@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 13:00:24 +0000

    color update

---
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/properties/GameColorWhack.java
@@ -0,0 +1,40 @@
+package thrust.entities.properties;
+
+import java.awt.Color;
+/**
+ * A color.
+ * @author Tara Flood (Tara.Flood@ucdconnect.ie)
+ * @version 21 April 2008
+ */
+public class GameColorWhack implements GameColor {
+//@ public model Color _the_color;
+  /**
+   * implementing my color.
+   */
+  Color my_color;
+  /**
+   * @return What color are you?
+   */
+  //@ ensures \result == _the_color;
+  public/*@ pure @*/ Color color() {
+    return my_color;
+  }
+
+  /**
+   * This is your color.
+   * @the_color the new color.
+   */
+  //@ ensures color() == the_color;
+  public void color(final Color the_color) {
+
+  }
+
+  //@ public invariant (* There are exactly 12 different colors available. *);
+  /*@ public invariant _the_color == Color.BLACK | _the_color == Color.WHITE |
+    @                  _the_color == Color.RED | _the_color == Color.GREEN |
+    @                  _the_color == Color.BLUE | _the_color == Color.CYAN |
+    @                  _the_color == Color.MAGENTA | _the_color == Color.DARK_GRAY |
+    @                  _the_color == Color.GRAY | _the_color == Color.LIGHT_GRAY |
+    @                  _the_color == Color.ORANGE | _the_color == Color.PINK;
+    @*/
+}
