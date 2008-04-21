34454922249aba572c66a9b3a85a31f88b906044
ne0dym1um <ne0dym1um@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 10:48:39 +0000

    Finished Version of Parser. All the values are how they are supposed to be. There is no other way of interpreting it. Big Endian!!!

--- COMP30050-2008/groups/kinephon/source/Parser/Parser.cpp
+++ COMP30050-2008/groups/kinephon/source/Parser/Parser.cpp
@@ -6,10 +6,28 @@ Parser::Parser()
 	for(int i=0; i<12; i++)
 		reportData[i] = 0;
 	for(int i=0; i<8; i++)
+	{
 		MSB_Size_Array[i] = 0;
+		binaryX[i] = 0;
+		binaryY[i] = 0;
+	}
 	for(int i=0; i<4; i++)
 		control[i] = 0;
+		
 }
+
+/*
+Parser::Parser(IParserRecorder* ipr)
+{
+	binaryPosition = 0;
+	for(int i=0; i<12; i++)
+		reportData[i] = 0;
+	for(int i=0; i<8; i++)
+		MSB_Size_Array[i] = 0;
+	for(int i=0; i<4; i++)
+		control[i] = 0;	
+}
+*/
 
 void Parser::parser(string in, int size_of)
 {
@@ -66,6 +84,7 @@ void Parser::parser(string in, int size_of)
 void Parser::provide()
 {
 	int tmp[3];
+	tmp[0]=0,tmp[1]=0,tmp[2]=0;
 	int c_switch=0;
 	for(int j=0;j<12;j+=3)
 	{
@@ -92,9 +111,11 @@ void Parser::provide()
 		  	for(int i=0;i<8;i++)
 		  		cout << MSB_Size_Array[i];
 		  	cout << endl;
-		  	tmp[0] += MSB_Extract(MSB_Size_Array[0],MSB_Size_Array[1]);
-		  	tmp[1] += MSB_Extract(MSB_Size_Array[2],MSB_Size_Array[3]);
+		  	tmp[0] += MSB_Extract(MSB_Size_Array[2],MSB_Size_Array[3]);
+		  	tmp[1] += MSB_Extract(MSB_Size_Array[0],MSB_Size_Array[1]);
 		  	tmp[2] = Size_Extract();
+		  	setX(tmp[0]);
+		  	setY(tmp[1]);
 		  	cout << " REPORTID: " << c_switch+1 << " X: " << tmp[0] << " Y: " << tmp[1] << " Size: " << tmp[2] <<  endl;
 		}
 	}
@@ -108,14 +129,16 @@ void Parser::provide()
 void Parser::binary(int number) {
 	int remainder;
 
-	if(number <= 1) {
+	if(number <= 1 && binaryPosition<=7) {
 		MSB_Size_Array[binaryPosition++] = number;
 		return;
 	}
-
-	remainder = number%2;
-	binary(number >> 1);    
-	MSB_Size_Array[binaryPosition++] = remainder;
+	else
+	{
+		remainder = number%2;
+		binary(number >> 1);    
+		MSB_Size_Array[binaryPosition++] = remainder;
+	}
 }
 
 int Parser::MSB_Extract(int first,int second)
--- COMP30050-2008/groups/kinephon/source/Parser/Parser.h
+++ COMP30050-2008/groups/kinephon/source/Parser/Parser.h
@@ -1,7 +1,7 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
-//#include "IParserRecorder.h"
+//#include "../rai/Recorder/IParserRecorder.h"
 #include <string>
 #include <iostream>
 #include <stdlib.h>
@@ -12,19 +12,21 @@ class Parser
 {
 public:
 	Parser();
-//	Parser(IParserRecorder);
+//	Parser(IParserRecorder* ipr);
 	void supply(string in){parser(in, in.size());};
+	int getX(){return x;};
+	int getY(){return y;};
 	virtual ~Parser();
 private:
 	void parser(string,int);
 	void provide();
 	void binary(int);
 	int MSB_Extract(int,int);
+	void invertBinaryValues(int,int);
 	int Size_Extract();
-	int reportData[12];
-	int MSB_Size_Array[8];
-	int control[4];
-	int binaryPosition;
+	void setX(int in){x=in;};
+	void setY(int in){y=in;};
+	int reportData[12], MSB_Size_Array[8], control[4], binaryPosition, x, y, binaryX[8], binaryY[8];
 };
 
 #endif /*PARSER_H_*/
