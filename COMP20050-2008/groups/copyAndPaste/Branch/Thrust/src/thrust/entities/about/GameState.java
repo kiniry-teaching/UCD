081246d5029960774208e640c3fcaec37dbae5b8
pNevin <pNevin@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 14:10:18 +0000

    cleaned this up too

---
+++ COMP20050-2008/groups/copyAndPaste/Branch/Thrust/src/thrust/entities/about/GameState.java
@@ -0,0 +1,192 @@
+package thrust.entities.about;
+/**
+ * The state of the Thrust game, including current score, bonus, fuel, lives,
+ * and high scores.
+ *
+ * @author Patrick Nevin (patrick.nevin@ucdconnect.ie)
+ * @version 20 April 2008
+ */
+public class GameState extends AbstractGameState {
+  /**
+   * The current bones.
+   */
+  private int my_bonus;
+  /**
+   * The current fuel.
+   */
+  private int my_fuel;
+  /**
+   * the max fuel.
+   */
+  private final int my_max_fuel;
+  /**
+   * the current score.
+   */
+  private int my_current_score;
+  /**
+   * current amount of lives.
+   */
+  private byte my_lives;
+  /**
+   * An array of High scores.
+   */
+  private HighScoreClass[] my_high_scores;
+  /**
+   * A constructor to initialise instances of this object.
+   * @param bonus
+   * @param fuel
+   * @param max_fuel
+   * @param score
+   * @param lives
+   */
+  public GameState(final int a_fuel, final int a_max_fuel, final byte a_lives) {
+    this.my_fuel = a_fuel;
+    this.my_max_fuel = a_max_fuel;
+    this.my_lives = a_lives;
+    this.my_high_scores = new HighScoreClass[HIGH_SCORE_COUNT];
+  }
+  /**
+   * @return What is the current bonus?
+   * @bon BONUS What is your value?
+   */
+  //@ ensures 0 <= \result;
+  public /*@ pure @*/ int bonus() {
+    return this.my_bonus;
+  }
+  /**
+   * @param the_new_value This is your new value.
+   */
+  //@ requires 0 <= the_new_value;
+  //@ ensures bonus() == the_new_value;
+  public void new_bonus(final int the_new_value) {
+    this.my_bonus = the_new_value;
+  }
+  /**
+   * @return How much fuel do you contain?
+   */
+  //@ ensures 0 <= \result;
+  public /*@ pure @*/ int current_fuel() {
+    return this.my_fuel;
+  }
+
+  /**
+   * @return How much fuel can you contain?
+   */
+  //@ ensures 0 <= \result;
+  public  /*@ pure @*/ int maximum_fuel() {
+    return this.my_max_fuel;
+  }
+
+  /**
+   * @return What is the current score?
+   */
+  //@ ensures 0 <= \result;
+  public  /*@ pure @*/ int score() {
+    return this.my_current_score;
+  }
+
+  /**
+   * Change the current score by this many points.
+   * @param some_new_points the new points to add to the current score.
+   */
+  //@ ensures score() = \old(score() + some_new_points);
+  public  void change_score(final int some_new_points) {
+    this.my_current_score += some_new_points;
+  }
+
+  /**
+   * @return How many lives do you have?
+   */
+  //@ ensures 0 <= \result;
+  public  /*@ pure @*/ byte lives() {
+    return this.my_lives;
+  }
+
+  /**
+   * @param some_new_lives Change the current lives by this many lives.
+   */
+  //@ ensures lives() == \old(lives() + some_new_lives);
+  public  /*@ pure @*/ void change_lives(final byte some_new_lives) {
+    this.my_lives += some_new_lives;
+  }
+
+  //@ public invariant HIGH_SCORE_COUNT == 8;
+  //@ public invariant (* There are eight high scores. *);
+
+  /**
+   * @return What are the current high scores?
+   */
+  //@ ensures \result.length == HIGH_SCORE_COUNT;
+  /*@ ensures (\for  all int i, int j; 0 <= i & i < j & j < HIGH_SCORE_COUNT &
+    @          \result[i] >= \result[j]);
+    @ ensures (* High scores are ordered from high to low. *);
+    @*/
+  public  /*@ pure \non null elements @*/ HighScoreInterface[] high_scores() {
+    return this.my_high_scores;
+  }
+
+  public HighScoreInterface high_score(final int the_index) {
+    return this.my_high_scores[the_index];
+  }
+
+  public boolean new_high_score(final HighScoreInterface the_pos_new_high) {
+    return (this.my_high_scores[0].my_score == the_pos_new_high.score());
+  }
+
+  public  void add_high_score(final HighScoreInterface the_new_high_score) {
+    for (int i = 0; i < HIGH_SCORE_COUNT; i++) {
+      if (this.my_high_scores[i].my_score <= the_new_high_score.score()) {
+        this.my_high_scores[i] = (HighScoreClass)the_new_high_score;
+      }
+    }
+  }
+  /**
+   * A pair of a sequence of three initials and a score.
+   *
+   * @author Patrick Nevin (Patrick Nevin: 06754155)
+   * @version 11 April 2008
+   */
+  private class HighScoreClass implements HighScoreInterface {
+    /**
+     *An array of size 3 to hold initials.
+     */
+    private char[] my_initialsArray = new char[MY_SIZE];
+    /**
+     *score associated with player with initials.
+     */
+    private int my_score;
+    /**
+     * @return What is your score?
+     */
+    //@ ensures 0 <= \result;
+    public /*@ pure @*/ int score() {
+      return this.my_score;
+    }
+
+    /**
+     * @return What are your initials?
+     */
+    //@ ensures \results.length == 3;
+    public /*@ pure \non-null-elements @*/ char[] initials() {
+      return this.my_initialsArray;
+    }
+
+    /**
+     * @param the_new_score This is your score.
+     */
+    //@ requires 0 <= the_new_score;
+    //@ ensures score() == the_new_score;
+    public void new_score(final int a_new_score) {
+      this.my_score = a_new_score;
+    }
+
+    /**
+     * @param the_new_initials These are your initials.
+     */
+    //@ requires the_new_intials.length == 3;
+    //@ ensures initials().equals(\old(the_new_initials));
+    public void new_initials(final/*@ non_null @*/ char[] a_new_initials) {
+      this.my_initialsArray = a_new_initials;
+    }
+  }
+}
