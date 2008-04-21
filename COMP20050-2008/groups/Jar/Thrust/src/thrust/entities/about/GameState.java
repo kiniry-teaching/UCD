013339d3e9dc5d45aeabd6f22ce6f82b763bf124
timbyr <timbyr@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-21 13:55:42 +0000

    Implemented some of the GameState

--- COMP20050-2008/groups/Jar/Thrust/src/thrust/entities/about/GameState.java
+++ COMP20050-2008/groups/Jar/Thrust/src/thrust/entities/about/GameState.java
@@ -9,34 +9,51 @@ package thrust.entities.about;
  */
 public class GameState extends AbstractGameState {
 
-  private int current_high_score;
+  /** The current score.*/
+  private int my_score;
+  /** The end of level bonus if any. */
+  private int my_bonus;
+  /** The amount of lives the player has. */
+  private byte my_lives;
+  /** The amount of fuel the ship has. */
+  private int my_fuel;
+  /** The high scores of the game. */
+  private HighScoreInterface[] my_high_scores = new HighScore[HIGH_SCORE_COUNT]; 
   /*
    * @see thrust.entities.about.AbstractGameState#
    *  add_high_score(thrust.entities.about.AbstractGameState.HighScoreInterface)
    */
   public void add_high_score(final HighScoreInterface the_new_high_score) {
-    
+    for (int i = 0; i < HIGH_SCORE_COUNT - 1; i++) {
+      if (my_high_scores[i] != null) {
+        if (my_score > my_high_scores[i].score() && i > 0) {
+          my_high_scores[i - 1] = my_high_scores[i];
+        } else {
+          my_high_scores[i] = the_new_high_score;
+        }
+      }
+    }
   }
 
   /*
    * @see thrust.entities.about.AbstractGameState#bonus()
    */
   public int bonus() {
-    return 0;
+    return my_bonus;
   }
 
   /*
    * @see thrust.entities.about.AbstractGameState#change_lives(byte)
    */
   public void change_lives(final byte some_new_lives) {
-
+    my_lives += some_new_lives;
   }
 
   /*
    * @see thrust.entities.about.AbstractGameState#change_score(int)
    */
   public void change_score(final int some_new_points) {
-
+    my_score += some_new_points;
   }
 
   /*
@@ -51,8 +68,7 @@ public class GameState extends AbstractGameState {
    * @see thrust.entities.about.AbstractGameState#high_score(int)
    */
   public HighScoreInterface high_score(final int the_index) {
-
-    return null;
+    return my_high_scores[the_index];
   }
 
   /*
@@ -60,7 +76,7 @@ public class GameState extends AbstractGameState {
    */
   public HighScoreInterface[] high_scores() {
 
-    return new HighScoreInterface[0];
+    return my_high_scores;
   }
 
   /*
@@ -68,7 +84,7 @@ public class GameState extends AbstractGameState {
    */
   public byte lives() {
 
-    return 0;
+    return my_lives;
   }
 
   /*
@@ -76,21 +92,26 @@ public class GameState extends AbstractGameState {
    */
   public int maximum_fuel() {
 
-    return 0;
+    return my_fuel;
   }
 
   /*
    * @see thrust.entities.about.AbstractGameState#new_bonus(int)
    */
   public void new_bonus(final int the_new_value) {
-
+    my_bonus = the_new_value;
   }
 
   /*
    * @see thrust.entities.about.AbstractGameState#new_high_score(thrust.entities.about.AbstractGameState.HighScoreInterface)
    */
   public boolean new_high_score(final HighScoreInterface the_high_score) {
-
+    for (int i = my_high_scores.length; i > 0; i--) {
+      if (my_high_scores[i] != null) {
+        if (my_score > my_high_scores[i].score())
+          return true;
+      }
+    }
     return false;
   }
 
@@ -99,7 +120,7 @@ public class GameState extends AbstractGameState {
    */
   public int score() {
 
-    return 0;
+    return my_score;
   }
 
 }
---
+++ COMP20050-2008/groups/Jar/Thrust/src/thrust/entities/about/HighScore.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package thrust.entities.about;
+
+import thrust.entities.about.AbstractGameState.HighScoreInterface;
+
+/**
+ * @author keith (timbyr@gmail.com)
+ * @version 21 April 2008
+ */
+public class HighScore implements HighScoreInterface {
+
+  /** Maximum number of initials that can be saved. */
+  private static final int INITIAL_NUMBER = 3;
+  /**The initials that have been saved. */
+  private char[] my_initials = new char[INITIAL_NUMBER];
+  /** The high score. */
+  private int my_high_score;
+
+  /*
+   * @see thrust.entities.about.AbstractGameState.HighScoreInterface#initials()
+   */
+  public char[] initials() {
+    return my_initials;
+  }
+
+  /*
+   * @see thrust.entities.about.AbstractGameState.HighScoreInterface#new_initials(char[])
+   */
+  public void new_initials(final char[] the_new_initials) {
+    my_initials = the_new_initials;
+  }
+
+  /*
+   * @see thrust.entities.about.AbstractGameState.HighScoreInterface#new_score(int)
+   */
+  public void new_score(final int the_new_score) {
+    my_high_score = the_new_score;
+  }
+
+  /*
+   * @see thrust.entities.about.AbstractGameState.HighScoreInterface#score()
+   */
+  public int score() {
+    return my_high_score;
+  }
+
+}
