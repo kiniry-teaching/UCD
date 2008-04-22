6f97d16d722e9d6517fa4aa593b5daecd2d047e7
magda <magda@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-22 12:05:13 +0000

    new classes

--- COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/audio/SoundEffect.java
+++ COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/audio/SoundEffect.java
@@ -1,3 +1,13 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
 package thrust.audio;
 
 import java.io.File;
@@ -13,7 +23,7 @@ public class SoundEffect {
    * @param the_sound_effect_file the sound effect to make.
    * @return the new sound effect for the effect stored in 's'.
    */
-  public /*@ pure @*/ SoundEffect make(File the_sound_effect_file) {
+  public static /*@ pure @*/ SoundEffect make(File the_sound_effect_file) {
     assert false; //@ assert false;
     return null;
   }
