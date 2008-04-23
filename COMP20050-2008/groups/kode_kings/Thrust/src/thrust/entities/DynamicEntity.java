119b8b65b369333a70e8213b6e598b7da358b39c
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-23 21:52:55 +0000

    Deleted KKPhysics and moved the PhysicsInterface implementation to DynamicEntity

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/DynamicEntity.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/DynamicEntity.java
@@ -14,9 +14,9 @@ import thrust.physics.PhysicsInterface;
 
 /**
  * Entities whose position or orientation change.
- * @author Joe
- *  Kiniry (kiniry@acm.org)
- * @version 18 April 2008
+ * @author Colin Casey (colin.casey@org.com)
+ * @author Joe Kiniry (kiniry@acm.org)
+ * @version 23 April 2008
  */
 public abstract class DynamicEntity extends Entity
   implements PhysicsInterface {
@@ -37,5 +37,79 @@ public abstract class DynamicEntity extends Entity
                                    final double[] the_velocity) {
     assert false; //@ assert false;
     return null;
+  }
+
+  /**
+   * Implementation of Physics methods.
+   * @author Colin Casey (colin.casey@org.com)
+   * @version 23 April 2008
+   */
+
+  /** The force that attracts the spaceship and
+   * goal sphere toward the terrain. */
+  private static final double GRAVITY_CONSTANT = -9.8;
+  /** Increase in the rate of speed of something. */
+  private static double[] my_acceleration;
+  /** The quantity of matter that an entity contains. */
+  private static double my_mass;
+  /** The quantity of motion of a moving entity. */
+  private static double my_momentum;
+  /** The speed of an entity in a given direction. */
+  private static double[] my_velocity;
+  /** The relative physical direction of entities. */
+  private static double my_orientation;
+  /** The co-ordinates where an entity is located. */
+  private static double[] my_position;;
+
+  public double[] acceleration() {
+    return my_acceleration;
+  }
+
+  public double gravitational_constant() {
+    return GRAVITY_CONSTANT;
+  }
+
+  public double mass() {
+    return my_mass;
+  }
+
+  public double momentum() {
+    return my_momentum;
+  }
+
+  public double orientation() {
+    return my_orientation;
+  }
+
+  public double[] position() {
+    return my_position;
+  }
+
+  public double[] velocity() {
+    return my_velocity;
+  }
+
+  public void position(final double[] the_position) {
+    my_position = the_position;
+  }
+
+  public void orientation(final double the_orientation) {
+    my_orientation = the_orientation;
+  }
+
+  public void mass(final double the_mass) {
+    my_mass = the_mass;
+  }
+
+  public void velocity(final double[] the_velocity) {
+    my_velocity = the_velocity;
+  }
+
+  public void acceleration(final double[] the_acceleration) {
+    my_acceleration = the_acceleration;
+  }
+
+  public void simulate(final double some_seconds) {
+
   }
 }
