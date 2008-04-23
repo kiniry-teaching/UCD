ce170fb765a63e8bb9ad2908a1017104b2cf65ff
06584551 <06584551@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-23 16:34:46 +0000

    Update

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/Star.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/Star.java
@@ -9,9 +9,14 @@
  */
 package thrust.entities.in_game;
 
+import java.awt.Color;
+import java.awt.Shape;
 import thrust.animation.Animatable;
+import thrust.animation.Animation;
 import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
+
+
 
 /**
  * A blinking star in space.
@@ -20,6 +25,95 @@ import thrust.entities.StaticEntity;
  */
 public class Star extends StaticEntity
   implements NeutralEntity, Animatable {
+  /**
+   *
+   * @author Allison Fallon(allison.fallon@ucdconnect.ie)
+   *@version 23 April 2008
+   */
+  Color my_starcolor;
+  /**
+   *
+   */
+  Shape my_starshape;
+
+  public double[] acceleration() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double mass() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double momentum() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] velocity() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void render() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public Shape shape() {
+    //Rectangle my_A = new Rectangle();
+
+    return null;
+  }
+
+  public void shape(final Shape the_shape) {
+
+
+  }
+
+  public String shape_name() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public byte state() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void state(final byte the_state) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void animate() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public Animation animation() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void animation(final Animation the_animation) {
+    // TODO Auto-generated method stub
+
+  }
+  public Color color() {
+    my_starcolor.equals(Color.WHITE);
+
+    return my_starcolor;
+
+  }
+
+  public void color(final Color the_color) {
+
+    my_starcolor = the_color;
+
+  }
+
   /*@ public invariant (* A star's location is in space. *);
     @ public invariant (* A star interacts with no other entities. *);
     @ public invariant (* Each star blinks irregularly. *);
