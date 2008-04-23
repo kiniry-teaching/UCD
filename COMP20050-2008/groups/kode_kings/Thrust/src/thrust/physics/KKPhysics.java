72c15a35d7ff755b3dabd5d6dff028dfa5313392
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-23 13:16:15 +0000

    Implemented new physics setter methods

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/KKPhysics.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/physics/KKPhysics.java
@@ -9,74 +9,68 @@ public class KKPhysics implements PhysicsInterface {
   /** The force that attracts the spaceship and
    * goal sphere toward the terrain. */
   private static final double GRAVITY_CONSTANT = -9.8;
+  /** Increase in the rate of speed of something. */
+  private static double[] my_acceleration;
   /** The quantity of matter that an entity contains. */
   private static double my_mass;
-  /** The rate of motion. */
-  private static double my_speed;
-  /** Change in the speed of something. */
-  private static double my_rate_of_speed;
-  /** Increase in the rate of speed of something. */
-  private static double[] my_acceleration = {my_speed, my_rate_of_speed};
+  /** The quantity of motion of a moving entity. */
+  private static double my_momentum;
+  /** The speed of an entity in a given direction. */
+  private static double[] my_velocity;
   /** The relative physical direction of entities. */
   private static double my_orientation;
-  /** The x co-ordinate where an entity is located. */
-  private static double my_position_x;
-  /** The y co-ordinate where an entity is located. */
-  private static double my_position_y;
+  /** The co-ordinates where an entity is located. */
+  private static double[] my_position;;
 
-  /**
-   * @return What is your acceleration in meters per second squared?
-   */
   public double[] acceleration() {
     return my_acceleration;
   }
 
-  /**
-   * @return What is the gravitational constant?
-   */
   public double gravitational_constant() {
     return GRAVITY_CONSTANT;
   }
 
-  /**
-   * @return What is your mass in kilogrammes?
-   */
   public double mass() {
     return my_mass;
   }
 
-  /**
-   * @return What is your momentum in kilogrammes*meters per second?
-   */
   public double momentum() {
-    return my_mass * my_speed;
+    return my_momentum;
   }
 
-  /**
-   * @return What is your orientation in radians?
-   */
   public double orientation() {
     return my_orientation;
   }
 
-  /**
-   * @return What is your position in meters from the origin?
-   */
   public double[] position() {
-    final double[] position = {my_position_x, my_position_y};
-    return position;
+    return my_position;
   }
 
-  /**
-   * @return What is your velocity in meters per second?
-   */
   public double[] velocity() {
-    final double[] velocity = {my_speed, my_orientation};
-    return velocity;
+    return my_velocity;
   }
 
-  public void simulate(final double some_seconds)
-  {
+  public void position(final double[] the_position) {
+    my_position = the_position;
+  }
+
+  public void orientation(final double the_orientation) {
+    my_orientation = the_orientation;
+  }
+
+  public void mass(final double the_mass) {
+    my_mass = the_mass;
+  }
+
+  public void velocity(final double[] the_velocity) {
+    my_velocity = the_velocity;
+  }
+
+  public void acceleration(final double[] the_acceleration) {
+    my_acceleration = the_acceleration;
+  }
+
+  public void simulate(final double some_seconds) {
 
   }
 }
