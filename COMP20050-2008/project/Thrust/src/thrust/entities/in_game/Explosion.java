eaf05ebb8b72d354d641bc8e8666eae4bc342c3c
fintan <fintan@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-23 23:39:24 +0000

    Restoring Explosion.java

---
+++ COMP20050-2008/project/Thrust/src/thrust/entities/in_game/Explosion.java
@@ -0,0 +1,24 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+package thrust.entities.in_game;
+
+import thrust.entities.NeutralEntity;
+import thrust.entities.StaticEntity;
+import thrust.animation.Animatable;
+
+/**
+ * An explosion.
+ * @author Joe Kiniry (kiniry@acm.org)
+ * @version 18 April 2008
+ */
+public class Explosion extends StaticEntity
+  implements NeutralEntity, Animatable {
+
+}
