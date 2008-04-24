000db82ed873a89c0556d7962773fcacf61e6a00
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 00:17:51 +0000

    Implemented Fuelable directly in SpaceShip. Deleted KKFuelable

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/Spaceship.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/Spaceship.java
@@ -34,7 +34,10 @@ public class Spaceship extends DynamicEntity
     @*/
   /** The spaceship's initial fuel is 1000 units. */
   public static final int INITIAL_FUEL = 1000;
-
+  /** The maximum amount of fuel that can be contained. */
+  private static final int MAX_FUEL = 10000;
+  /** The amount of fuel that is contained. */
+  private static int my_fuel;
   /** Tow status of Spaceship. */
   private boolean my_tow_indicator;
 
@@ -44,6 +47,26 @@ public class Spaceship extends DynamicEntity
 
   public void tow() {
     my_tow_indicator = true;
+  }
+
+  public int fuel() {
+    return my_fuel;
+  }
+
+  public int maximum_fuel() {
+    return MAX_FUEL;
+  }
+
+  public void set_fuel_content(final int the_fuel_content) {
+    my_fuel = the_fuel_content;
+  }
+
+  public void change_fuel_content(final int the_fuel_change) {
+    my_fuel = my_fuel + the_fuel_change;
+  }
+
+  public int fuel_mass() {
+    return my_fuel;
   }
 
   //@ public initially mass() == EMPTY_MASS + INITIAL_FUEL;
