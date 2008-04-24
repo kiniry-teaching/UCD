5d9ad743502057a54ede1b339f1d9c137f1af0d6
ne0dym1um <ne0dym1um@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 09:25:43 +0000

    I commented all of my code. I took out any of the old methods for testing. From what I can see this is the final version.

--- COMP30050-2008/groups/kinephon/source/Parser/Parser.cpp
+++ COMP30050-2008/groups/kinephon/source/Parser/Parser.cpp
@@ -9,8 +9,6 @@ Parser::Parser()
 	for(int i=0; i<8; i++)
 	{
 		MSB_Size_Array[i] = 0;
-		binaryX[i] = 0;
-		binaryY[i] = 0;
 	}
 	for(int i=0; i<4; i++)
 		control[i] = 0;
@@ -28,62 +26,6 @@ Parser::Parser(IParserRecorder* ipr)
 		MSB_Size_Array[i] = 0;
 	for(int i=0; i<4; i++)
 		control[i] = 0;	
-}
-
-/*
- * This is just used for parsing the test data.
- */
-
-void Parser::parser(string in, int size_of)
-{
-	int counter = 0;
-	for(int i=0; i+4<size_of; i++)
-	{
-		if(in[i]=='[' && in[i+2]==',')
-		{
-			reportData[counter] = atoi(in.substr(i+1, i+1).data());
-			counter++;
-		}
-		else
-		{
-			if(in[i]=='[' && in[i+3]==',')
-			{
-				reportData[counter] = atoi(in.substr(i+1, i+2).data());
-				counter++;
-			}
-			else
-			{
-				if(in[i]=='[' && in[i+4]==',')
-				{
-					reportData[counter] = atoi(in.substr(i+1, i+3).data());
-					counter++;
-				}	
-			}
-		}
-
-		if(in[i]==' ' && in[i+2]==',')
-		{
-			reportData[counter] = atoi(in.substr(i+1, i+1).data());
-			counter++;
-		}
-		else
-		{
-			if(in[i]==' ' && in[i+3]==',' || in[i+3]==']')
-			{
-				reportData[counter] = atoi(in.substr(i+1, i+2).data());
-				counter++;
-			}
-			else
-			{
-				if(in[i]==' ' && in[i+4]==',' || in[i+4]==']')
-				{
-					reportData[counter] = atoi(in.substr(i+1, i+3).data());
-					counter++;
-				}	
-			}
-		}
-	}
-//		provide();
 }
 
 void Parser::supplyReport(IRReport report)
@@ -105,58 +47,36 @@ void Parser::supplyReport(IRReport report)
 		if(tmp[0]==255 || tmp[1]==255 || tmp[2]==255)
 		{
 			control[c_switch]=1;
-						iprp->control(control[c_switch],c_switch+1);
+			iprp->control(control[c_switch],c_switch+1);
 		}
 		else
 		{
 			control[c_switch]=0;
-						iprp->control(control[c_switch],c_switch+1);
+			iprp->control(control[c_switch],c_switch+1);
 		}
 
 		if(control[c_switch]==0)
 		{
-			for (int i = 0 ; i < 8; i++) {
+			for(int i = 0 ; i < 8; i++)
+			{
 				MSB_Size_Array[i] = 0;
 			}
 			
 			populate(tmp[2]);
-			//binary(tmp[2]);
-			for(int i=0;i<8;i++)
-				cout << MSB_Size_Array[i];
-			cout << "";
 			tmp[0] += MSB_Extract(MSB_Size_Array[2],MSB_Size_Array[3]);
 			tmp[1] += MSB_Extract(MSB_Size_Array[0],MSB_Size_Array[1]);
 			tmp[2] = Size_Extract();
-			cout << " REPORTID: " << c_switch+1 << " X: " << tmp[0] << " Y: " << tmp[1] << " Size: " << tmp[2];
-					  	iprp->record(c_switch+1, tmp[0], tmp[1], tmp[2], report.getTimestamp());
+			iprp->record(c_switch+1, tmp[0], tmp[1], tmp[2], report.getTimestamp());
 		}
-	}
-	//	for(int k=0;k<4;k++)
-	//	{
-	//		cout << control[k] << " ";
-	//	}
-	//	cout << endl;
-}
-
-void Parser::binary(int number) {
-	int remainder;
-
-	if(number <= 1 && binaryPosition<=7) {
-		MSB_Size_Array[binaryPosition++] = number;
-		return;
-	}
-	else
-	{
-		remainder = number%2;
-		binary(number >> 1);    
-		MSB_Size_Array[binaryPosition++] = remainder;
 	}
 }
 
 void Parser::populate(int number) {
 	int array[] = {128, 64, 32, 16, 8, 4, 2, 1};
-	for (int i=0; i<=7; i++) {
-		if (number >= array[i]) {
+	for (int i=0; i<=7; i++)
+	{
+		if (number >= array[i])
+		{
 			MSB_Size_Array[i] = 1;
 			number -= array[i];
 		}
@@ -174,16 +94,11 @@ int Parser::MSB_Extract(int first,int second)
 
 int Parser::Size_Extract()
 {
-	int counterWeight=0,total=0;
+	int total=0;
 	for(int i=4;i<7;i++)
 	{
 		if(MSB_Size_Array[i]==1)
-			total+=(int)pow(2.0,counterWeight);
-		counterWeight++;
+			total+=(int)pow(2.0,i);
 	}
 	return total;	
-}
-
-Parser::~Parser()
-{
 }
--- COMP30050-2008/groups/kinephon/source/Parser/Parser.h
+++ COMP30050-2008/groups/kinephon/source/Parser/Parser.h
@@ -1,3 +1,8 @@
+/*
+ * Software Engineering Project III
+ * David Swords 05477689
+ */
+
 #ifndef PARSER_H_
 #define PARSER_H_
 
@@ -15,20 +20,56 @@ class Parser
 {
 public:
 	Parser();
+	/*
+	 * This constructor accepts a IParserRecorder pointer.
+	 * This pointer is used to pass the necessary information
+	 * from the wiimote to the analyser.
+	 */
 	Parser(IParserRecorder* ipr);
-	//The format of supply will be changed to accept an array
-	void supply(string in){parser(in, in.size());};
+	/*
+	 * This method does the work.
+	 * First we check the vector of decimal values look to
+	 * see if any of them are providing no information. It can
+	 * then pass this information on to the analyser so it can
+	 * see what points are active.
+	 * It then works on a set of three values, each one is a report.
+	 * The third value of the report has the following structure
+	 * 
+	 * 		yyxxssss
+	 * 
+	 * The first two bits of the byte are the most significant bits of the
+	 * Y coordinate, the next two are the MSB of the X coordinate. The last
+	 * four is the size value of the point. All of these are big endian.
+	 * The values extracted are then added to their decimal values and passed
+	 * on to the analyser.
+	 */
 	void supplyReport(IRReport);
-	virtual ~Parser();
 private:
-	void parser(string,int);
-	void provide();
-	void binary(int);
+	/*
+	 * This is the function I use to turn the third byte
+	 * into a binary value so I can extract the additional information.
+	 * It works by taking in a integer, checking to see if it greater than
+	 * or equal to a 2^n value, if it is then an entry is set to one. It
+	 * repeats this cycle an subtracts the last 2^n from the current. The
+	 * array being used for the values is global.
+	 */
 	void populate(int);
+	/*
+	 * This code takes one of the MSB values and converts it into a
+	 * decimal value.
+	 */
 	int MSB_Extract(int,int);
+	/*
+	 * This is a function that simply takes that last four values in
+	 * the binary array and turns that into a decimal number.
+	 */
 	int Size_Extract();
+	/*
+	 * Just a global pointer which I will use for the pointer that is passed
+	 * in the constructor.
+	 */
 	IParserRecorder* iprp;
-	int reportData[12],MSB_Size_Array[8],control[4],binaryPosition,binaryX[8], binaryY[8];
+	int reportData[12],MSB_Size_Array[8],control[4],binaryPosition;
 };
 
 #endif /*PARSER_H_*/
