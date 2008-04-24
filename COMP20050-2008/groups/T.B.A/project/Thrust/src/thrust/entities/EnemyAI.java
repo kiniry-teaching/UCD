6c1613138898295940a441b08e3835ed99e9b416
Percy <Percy@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 23:28:44 +0000

    Just empty methods, not sure if we need to write the behaviour methods

---
+++ COMP20050-2008/groups/T.B.A/project/Thrust/src/thrust/entities/EnemyAI.java
@@ -0,0 +1,31 @@
+package thrust.entities;
+
+import thrust.entities.behaviors.AI;
+/**
+ * The AI for the enemy entities.
+ * @author Eoin Healy (eoin.healy@gmail.com)
+ * @version 24 April 2008
+ */
+public class EnemyAI implements EnemyEntity {
+
+  public AI attack() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void attack(final AI the_behavior) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public AI disturb() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void disturb(final AI the_behavior) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
