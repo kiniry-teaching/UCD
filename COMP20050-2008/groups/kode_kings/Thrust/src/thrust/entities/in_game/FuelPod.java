08beddf984c652ae02c00388d30640caea5a57b9
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 00:43:59 +0000

    Implemented Fuelable methods in FuelPod

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/FuelPod.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/FuelPod.java
@@ -15,13 +15,37 @@ import thrust.entities.about.Fuelable;
 
 /**
  * A fuel pod from which the spaceship can obtain fuel.
+ * @author Colin Casey (colin.casey@org.com)
  * @author Joe Kiniry (kiniry@acm.org)
- * @author ciaranhale (ciaran.hale@ucdconnect.ie)
  * @version 18 April 2008
  */
 public class FuelPod extends StaticEntity
   implements NeutralEntity, Fuelable {
 
+  /** The maximum amount of fuel that can be contained. */
+  private static final int MAX_FUEL = 1000;
+  /** The amount of fuel that is contained. */
+  private static int my_fuel;
+
+  public int fuel() {
+    return my_fuel;
+  }
+
+  public int maximum_fuel() {
+    return MAX_FUEL;
+  }
+
+  public void set_fuel_content(final int the_fuel_content) {
+    my_fuel = the_fuel_content;
+  }
+
+  public void change_fuel_content(final int the_fuel_change) {
+    my_fuel = my_fuel + the_fuel_change;
+  }
+
+  public int fuel_mass() {
+    return my_fuel;
+  }
   /*@ public invariant (* A fuel pod is destroyed by a bullet. *);
     @ public invariant (* The fuel pod is not affected by the goal sphere. *);
     @ public invariant (* The fuel pod is not affected by the spaceship. *);
