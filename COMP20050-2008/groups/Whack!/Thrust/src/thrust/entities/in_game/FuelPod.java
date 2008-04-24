ea42fa18a23dda3d84984ca41996f4bf280a585e
06584551 <06584551@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 14:40:16 +0000

    updated shape

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/FuelPod.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/FuelPod.java
@@ -16,99 +16,187 @@ import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
 import thrust.entities.about.Fuelable;
 
+
 /**
  * A fuel pod from which the spaceship can obtain fuel.
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 18 April 2008
  */
+
 public class FuelPod extends StaticEntity
     implements NeutralEntity, Fuelable {
   /**
    * @author allison fallon(allison.fallon@ucdconnect.ie)
    */
+  Color my_fuelpodcolor;
+  /**
+   *
+   */
+  Shape my_fuelpodshape;
 
   public double[] acceleration() {
-    // TODO Auto-generated method stub
+
     return null;
   }
 
   public double mass() {
-    // TODO Auto-generated method stub
+
     return 0;
   }
 
   public double momentum() {
-    // TODO Auto-generated method stub
+
     return 0;
   }
 
   public double[] velocity() {
-    // TODO Auto-generated method stub
+
     return null;
   }
 
   public void render() {
-    // TODO Auto-generated method stub
+
+
+  }
+
+  /**
+   *
+   */
+  public class Point {
+    /**
+     *
+     */
+    int my_rx = 1;
+    /**
+     *
+     */
+    int my_ry = 1;
+
+    public Point(final int the_rx, final int the_ry) {
+
+      my_rx = the_rx;
+      my_ry = the_ry;
+
+    }
+
+  }
+  /**
+   *
+   */
+  public class Rectangle {
+    /**
+     * width of rectangle.
+     */
+    final int my_width = 0;
+    /**
+     * height of rectangle.
+     */
+    final int my_height = 0;
+    /**
+     *
+     */
+    final Point my_org;
+
+    public Rectangle() {
+
+      my_org = new Point(0, 0);
+
+    }
+
+    public Rectangle(final Point the_p) {
+
+      my_org = the_p;
+
+    }
+    public Rectangle(final int the_w, final int the_h) {
+
+      my_org = new Point(0, 0);
+      my_width = the_w;
+      my_height = the_h;
+
+    }
+    public Rectangle(final Point the_p, final int the_w, final int the_h) {
+
+      my_org = the_p;
+      my_width = the_w;
+      my_height = the_h;
+
+    }
 
   }
 
   public Shape shape() {
-    // TODO Auto-generated method stub
-    return null;
+    /**
+     *
+     */
+    final Rectangle my_a = new Rectangle(10, 40);
+
+    my_fuelpodshape = (Shape)my_a;
+
+    return my_fuelpodshape;
+
   }
 
   public void shape(final Shape the_shape) {
-    // TODO Auto-generated method stub
+
+    my_fuelpodshape = the_shape;
 
   }
 
+
   public String shape_name() {
-    // TODO Auto-generated method stub
+
     return null;
+
   }
 
   public byte state() {
-    // TODO Auto-generated method stub
+
     return 0;
+
   }
 
   public void state(final byte the_state) {
-    // TODO Auto-generated method stub
+
 
   }
 
   public void change_fuel_content(final int the_fuel_change) {
-    // TODO Auto-generated method stub
+
 
   }
 
   public int fuel() {
-    // TODO Auto-generated method stub
+
     return 0;
+
   }
 
   public int fuel_mass() {
-    // TODO Auto-generated method stub
+
     return 0;
+
   }
 
   public int maximum_fuel() {
-    // TODO Auto-generated method stub
+
     return 0;
+
   }
 
   public void set_fuel_content(final int the_fuel_content) {
-    // TODO Auto-generated method stub
+
 
   }
 
   public Color color() {
-    // TODO Auto-generated method stub
-    return null;
+    my_fuelpodcolor.equals(Color.YELLOW);
+    return my_fuelpodcolor;
   }
 
   public void color(final Color the_color) {
-    // TODO Auto-generated method stub
+
+    my_fuelpodcolor = the_color;
   }
   /*@ public invariant (* A fuel pod is destroyed by a bullet. *);
     @ public invariant (* The fuel pod is not affected by the goal sphere. *);
