ff5230aaa62b45367e325672fa28ef0eb27aadba
ciaranhale <ciaranhale@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 17:00:48 +0000

    Original Interface EnemyEntity

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/EnemyEntity.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/EnemyEntity.java
@@ -1,4 +1,3 @@
-
 /*
  * A re-implementation of the classic C=64 game 'Thrust'.
  *
@@ -18,92 +17,26 @@ import thrust.entities.behaviors.AI;
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 18 April 2008
  */
-public class EnemyEntity extends AI {/*
-	 * A re-implementation of the classic C=64 game 'Thrust'.
-	 *
-	 * @author "Joe Kiniry (kiniry@acm.org)"
-	 * @module "COMP 20050, COMP 30050"
-	 * @creation_date "March 2007"
-	 * @last_updated_date "April 2008"
-	 * @keywords "C=64", "Thrust", "game"
-	 */
-
-	package thrust.entities;
-
-	import thrust.entities.behaviors.AI;
-
-	/**
-	 * An entity that is a threat to the spaceship.
-	 * @author Joe Kiniry (kiniry@acm.org)
-	 * @version 18 April 2008
-	 */
-	public interface EnemyEntity {
-	  /**
-	   * @return What is your attack behavior AI?
-	   */
-	  /*@ pure @*/ AI attack();
-
-	  /**
-	   * @return What is your disturb behavior AI?
-	   */
-	  /*@ pure @*/ AI disturb();
-
-	  /**
-	   * @param the_behavior This is your attack behavior.
-	   */
-	  //@ ensures attack() == the_behavior;
-	  void attack(AI the_behavior);
-
-	  /**
-	   * @param the_behavior This is your disturb behavior.
-	   */
-	  //@ ensures disturb() == the_behavior;
-	  void disturb(AI the_behavior);
-	}
-
-
-    /**
-     * implements disturb.
-     */
-
-  private AI my_disturb_status();
-   /**
-    * implements attack.
-    */
-
-  private AI my_attack_status();
-
+public interface EnemyEntity {
   /**
    * @return What is your attack behavior AI?
    */
-  public/*@ pure @*/ AI attack(); {
+  /*@ pure @*/ AI attack();
 
-    return my_attack_status;
-  }
   /**
    * @return What is your disturb behavior AI?
    */
-  public/*@ pure @*/ AI disturb(); {
-
-    return my_disturb_status;
-  }
+  /*@ pure @*/ AI disturb();
 
   /**
    * @param the_behavior This is your attack behavior.
    */
   //@ ensures attack() == the_behavior;
-  public void attack(AI the_behavior); {
-
-   my_attack_status = the_behavior;
-  }
+  void attack(AI the_behavior);
 
   /**
    * @param the_behavior This is your disturb behavior.
    */
   //@ ensures disturb() == the_behavior;
-public  void disturb(AI the_behavior); {
-
-my_disturb_status the_behavior;
-
-  }
+  void disturb(AI the_behavior);
 }
