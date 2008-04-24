9cf86ed61af16d71dfd68cb28c32c5133db82fbe
Tara <Tara@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-24 13:30:20 +0000

    update

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/physics/Physicswhack.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/physics/Physicswhack.java
@@ -8,12 +8,13 @@ package thrust.physics;
  * @version 8 April 2008
  */
 
-public class Physicswhack implements Physics {
+public class Physicswhack implements PhysicsInterface {
+
   /**
    * Variable X in Position.
    */
   double my_x;
-   /**
+  /**
    *  Variable Y in Position.
    */
   double my_y;
@@ -93,6 +94,55 @@ public class Physicswhack implements Physics {
    * @param a_time_interval the amount of time that has passed.
    */
   public void simulate(final double a_time_interval) {
+  }
+  /**
+   * @param the_acceleration This is your acceleration.
+   */
+  //@ requires the_acceleration.length == 2;
+  //@ ensures acceleration()[0] == the_acceleration[0];
+  //@ ensures acceleration()[1] == the_acceleration[1];
+  public void acceleration(final double[] the_acceleration) {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * @param the_mass This is your mass.
+   */
+  //@ requires 0 <= the_mass;
+  //@ ensures mass() == the_mass;
+  public void mass(final double the_mass) {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * @param the_orientation This is your orientation.
+   */
+  //@ ensures orientation() == the_orientation;
+  public void orientation(final double the_orientation) {
+    // TODO Auto-generated method stub
+
+  }
+  /**
+   * @param the_position This is your position.
+   */
+  //@ requires the_position.length == 2;
+  //@ ensures position()[0] == the_position[0];
+  //@ ensures position()[1] == the_position[1];
+  public void position(final double[] the_position) {
+    // TODO Auto-generated method stub
+
+  }
+  /**
+   * @param the_velocity This is your velocity.
+   */
+  //@ requires the_velocity.length == 2;
+  //@ ensures velocity()[0] == the_velocity[0];
+  //@ ensures velocity()[1] == the_velocity[1];
+  public void velocity(final double[] the_velocity) {
+    // TODO Auto-generated method stub
+
   }
 
 }
