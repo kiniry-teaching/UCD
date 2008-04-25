df7b5c603a38f03a3508e712d562357aaa8d0e69
magda <magda@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-25 12:44:35 +0000

    FuelableClass-with bugs

---
+++ COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/about/FuelableClass.java
@@ -0,0 +1,81 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
+package thrust.entities.about;
+
+/**
+ * Implementation of Fuelable interface.
+ * @author Magdalena Zieniewicz (mazienie@gmail.com)
+ * @version 24 April 2008
+ */
+public class FuelableClass implements Fuelable {
+  
+  /** Volume of fuel in the tank in liters. */
+  private int my_fuel;
+  /** Capacity of fuel tank. */
+  private int my_max_fuel;
+  
+ 
+  
+  
+  
+  /**
+   * @param the_fuel_change Change your fuel content by this many units.
+   */
+  /*@ ensures (\old(fuel() + the_fuel_change < 0) ?
+    @            (fuel() == 0) :
+    @          (\old(maximum_fuel() < (fuel() + the_fuel_change)) ?
+    @             (fuel() == maximum_fuel()) :
+    @           fuel() == \old(fuel() + the_fuel_change)));
+    @*/
+
+
+  //@ public invariant (* Fuel content is always non-negative and finite. *);
+  //@ public invariant 0 <= fuel();
+
+  //@ public invariant (* One unit of fuel weights 1kg. *);
+
+  public void change_fuel_content(int the_fuel_change) {
+   int fuel = fuel() + the_fuel_change;
+   my_fuel = fuel;
+  }
+
+  /**
+   * @return How much fuel do you contain?
+   */
+  public int fuel() {
+    return my_fuel;
+  }
+
+  /**
+   * @return What is the mass of your fuel?
+   */
+  public int fuel_mass() {
+    return fuel() * 1;
+  }
+
+  /**
+   * @return How much fuel can you contain?
+   */
+  public int maximum_fuel() {
+    return my_max_fuel;
+  }
+
+  
+  /**
+   * @param the_fuel_content This many units is your fuel content.
+   */
+  //@ requires 0 <= the_fuel_content & the_fuel_content <= maximum_fuel();
+  //@ ensures fuel() == the_fuel_content;
+  public void set_fuel_content(int the_fuel_content) {
+   my_fuel = the_fuel_content;
+  }
+
+}
