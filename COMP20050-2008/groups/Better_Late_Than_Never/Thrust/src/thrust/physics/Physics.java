36c348590c633deaacead7dc64292c997a9d8214
nickm <nickm@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 18:42:05 +0000

    Replaced old Physics class with Physics2 (renamed to Physics), minor change to PhysicsInterface RE: double[] velocity

--- COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/physics/Physics.java
+++ COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/physics/Physics.java
@@ -1,80 +1,128 @@
 package thrust.physics;
-/*
- * Taking a look at some of the physics stuff..
- * @author: nm
- * @date: 22-04-08
+
+/** Class implementation of the PhysicsInterface.
+ * @author Nicholas McCarthy (nicholas.mccarthy@gmail.com)
+ * @version 27 April 2008
  */
 public class Physics implements PhysicsInterface {
-  /** The gravitational constant. */
-  public static final double the_grav_constant = 9.8;
-  /** Current acceleration. */
-  private double my_acceleration;
-  /** Current mass. */
+
+  /** Array holding acceleration and orientation of Entity. */
+  private double[] my_acceleration;
+  /** Double holding the gravitational constant value. */
+  private final double my_gravitational_constant = 0;
+  /** Double holding current mass of the ship. */
   private double my_mass;
-  /** Current momentum. */
+  /** Double holding current momentum of the Entity. */
   private double my_momentum;
-  /** Current orientation (radians). */
+  /** Double holding orientation of ship in radians. */
   private double my_orientation;
-  /** Current x coordinate of entity. */
-  private double my_xposition;
-  /** Current y coordinate of entity. */
-  private double my_yposition;
-  /** Current velocity. */
-  private double my_velocity;
-  /** Because 2 is a magic number..*/
-  private final int my_magicnumber = 2;
+  /** Array of doubles holding X and Y coordinates of Entity. */
+  private double[] my_position;
+  /** Array of doubles holding velocity and orientation of Entity. */
+  private double[] my_velocity;
+  /** Int for use in my_acceleration, my_position and my_velocity arrays. */
+  private final int my_arraylength = 2;
+  /** Double holding amount of seconds to simulate for. */
+  private double my_seconds;
 
-
-
+  /** Returns current acceleration of the Entity. */
   public double[] acceleration() {
-	  double[] accel = new double[2];
-	  accel[0] = my_acceleration;
-	  accel[1] = my_velocity
-	  return accel[];
+    return my_acceleration;
   }
 
+/** Returns the gravitational constant in use. */
   public double gravitational_constant() {
-    // Doesn't change or need to be private, so public final? - confirm
-
-    return the_grav_constant;
+    return my_gravitational_constant;
   }
 
+  /** Returns current mass of the Entity in kilograms. */
   public double mass() {
-
     return my_mass;
   }
 
+/** Returns momentum of the Entity in kilograms*meters per second. */
   public double momentum() {
-    // p = m * v
-    my_momentum = my_mass * my_velocity;
+ // Assuming my_velocity[0] is current speed
+    my_momentum = my_mass * my_velocity[0];
     return my_momentum;
   }
 
+/** Returns orientation of the Entity in radians. */
   public double orientation() {
-    // TODO Auto-generated method stub
     return my_orientation;
   }
 
+/** Returns position in metres from the origin of the Entity. */
   public double[] position() {
-    final double[] position = new double[my_magicnumber];
-    position[0] = my_xposition;
-    position[1] = my_yposition;
+    return my_position;
+  }
 
-    return position();
+  // See: void velocity(the_velocity) method.
+  // Changed from double[] to double here and in PhysicsInterface.
+/** Returns current velocity of the Entity in metres per second. */
+  public double velocity() {
+    return my_velocity[0];
+  }
+
+  /** Calculates new acceleration of Entity. */
+  public void acceleration(final double[] the_acceleration) {
+
+    // If statement using my_arraylength because it equals 2
+    // and 2 is a magic number.
+    if (the_acceleration.length == my_arraylength) {
+      my_acceleration = new double[my_arraylength];
+      my_acceleration[0] = the_acceleration[0];
+      my_acceleration[1] = the_acceleration[1];
+    } else {
+      System.out.print("the_acceleration.length must equal 2");
+    }
+  }
+
+  /** Ensures mass is positive, equal to the_mass. */
+  public void mass(final double the_mass) {
+
+    if (the_mass < 0) {
+      System.out.print("Mass cannot be negative.");
+    } else {
+      my_mass = the_mass;
+    }
+  }
+
+/** Ensures orientatian is equal to the_orientation. */
+  public void orientation(final double the_orientation) {
+
+    my_orientation = the_orientation;
+  }
+
+/** Ensures positions match up, the_position.length must equal 2. */
+  public void position(final double[] the_position) {
+
+    if (the_position.length == my_arraylength) {
+      my_position = new double[my_arraylength];
+      my_position[0] = the_position[0];
+      my_position[1] = the_position[1];
+    } else {
+      System.out.print("the_position.length must equal 2");
+    }
+  }
+
+  // Assuming the_velocity[0] == current speed
+  // and the_velocity[1] == current orientation
+/** Ensures velocities match up, the_velocity.length must equal 2. */
+  public void velocity(final double[] the_velocity) {
+
+    if (the_velocity.length == my_arraylength) {
+      my_velocity = new double[my_arraylength];
+      my_velocity[0] = the_velocity[0];
+      my_velocity[1] = the_velocity[1];
+    } else {
+      System.out.print("the_velocity.length must equal 2");
+    }
   }
 
   public void simulate(final double some_seconds) {
-    // TODO Auto-generated method stub
 
-  }
-
-  public double[] velocity() {
-    // Speed in a given direction (orientation)
-    final double[] velocity = new double[my_magicnumber];
-    velocity[0] = my_velocity;
-    velocity[1] = my_orientation;
-
-    return velocity();
+    my_seconds = some_seconds;
   }
 
 }
