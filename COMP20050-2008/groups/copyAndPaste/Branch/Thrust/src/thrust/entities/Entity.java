14cd82302f7a9a47696e6e953d4e68176c7bbae9
pNevin <pNevin@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 13:33:57 +0000

    Deleted Entity class and provided implementation here,

--- COMP20050-2008/groups/copyAndPaste/Branch/Thrust/src/thrust/entities/Entity.java
+++ COMP20050-2008/groups/copyAndPaste/Branch/Thrust/src/thrust/entities/Entity.java
@@ -11,68 +11,73 @@
 package thrust.entities;
 
 import thrust.entities.properties.GameColor;
+import java.awt.Color;
 import java.awt.Shape;
 
 /**
  * Any entity in the game that is drawn in space or on the terrain.
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 18 April 2008
+ * @revised P Nevin (patrick.nevin@ucdconnect.ie)
+ * @version 27 April 2008
  */
 public abstract class Entity implements GameColor {
+  /**The initial shape name.*/
+  private String my_shapeName;
+  /**The initial shape.*/
+  private Shape my_shape;
+  /**The initial state.*/
+  private byte my_state;
+  /**The color of the Entity.*/
+  private Color my_color;
   /**
    * Set the initial shape name, shape, and state of this entity.
    * @param the_initial_shape_name the initial shape name.
    * @param the_initial_shape the initial shape.
    * @param the_initial_state the initial state.
    */
-  public void set_state(String the_initial_shape_name,
-                        Shape the_initial_shape,
-                        byte the_initial_state) {
-    assert false; //@ assert false;
+  public void set_state(final String the_initial_shape_name,
+                        final Shape the_initial_shape,
+                        final byte the_initial_state) {
+    this.my_shapeName = the_initial_shape_name;
+    this.my_shape = the_initial_shape;
+    this.my_state = the_initial_state;
   }
-
   /**
    * @return What shape are you?
    */
   public /*@ pure @*/ String shape_name() {
-    assert false; //@ assert false;
-    return null;
+    return this.my_shapeName;
   }
-
   /**
    * @return What shape are you?
    */
   public /*@ pure @*/ Shape shape() {
-    assert false; //@ assert false;
-    return null;
+    return this.my_shape;
   }
-
   /**
    * This is your shape.
    * @param the_shape the shape of this Entity.
    */
-  public void shape(Shape the_shape) {
-    assert false; //@ assert false;
+  public void shape(final Shape the_shape) {
+    this.my_shape = the_shape;
   }
-
   /**
    * @return What is your physical state?
    * @note State is encoded by a non-negative number of "hit points".
    */
   //@ ensures 0 <= \result;
   public /*@ pure @*/ byte state() {
-    assert false; //@ assert false;
-    return 0;
+    return this.my_state;
   }
-
   /**
    * This is your physical state.
    * @param the_state the state.
    */
   //@ requires 0 <= the_state;
   //@ ensures state() == the_state;
-  public void state(byte the_state) {
-    assert false; //@ assert false;
+  public void state(final byte the_state) {
+    this.my_state = the_state;
   }
 
   /**
@@ -81,4 +86,13 @@ public abstract class Entity implements GameColor {
   public void render() {
     assert false; //@ assert false;
   }
+
+  public Color color() {
+    return this.my_color;
+  }
+
+  public void color(final Color the_color) {
+    this.my_color = the_color;
+  }
+
 }
