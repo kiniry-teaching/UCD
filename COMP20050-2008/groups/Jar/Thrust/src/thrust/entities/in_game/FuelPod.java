140c9d9803d924cd2eba858474baf133f0685bec
srussell <srussell@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 19:00:16 +0000

    Implemented fuelable

--- COMP20050-2008/groups/Jar/Thrust/src/thrust/entities/in_game/FuelPod.java
+++ COMP20050-2008/groups/Jar/Thrust/src/thrust/entities/in_game/FuelPod.java
@@ -20,10 +20,48 @@ import thrust.entities.about.Fuelable;
  */
 public class FuelPod extends StaticEntity
   implements NeutralEntity, Fuelable {
-  /** The amount of remaining in the feul pod. */
+  /** The amount of fuel currently in the FuelPod. */
   private int my_fuel;
+  /** The maximum amount of fuel that a FuelPod can hold. */
+  private int my_max_fuel;
+  /* (non-Javadoc)
+   * @see thrust.entities.about.Fuelable#change_fuel_content(int)
+   */
+  public void change_fuel_content(final int the_fuel_change) {
+    if (my_fuel + the_fuel_change < 0) {
+      my_fuel = 0;
+    } else if (maximum_fuel() < (my_fuel + the_fuel_change)) {
+      my_fuel = maximum_fuel();
+    } else {
+      my_fuel += the_fuel_change;
+    }
+  }
 
-  void set_fuel_content(int the_fuel_content) {
+  /* (non-Javadoc)
+   * @see thrust.entities.about.Fuelable#fuel_mass()
+   */
+  public int fuel_mass() {
+    return my_fuel;
+  }
+
+  /* (non-Javadoc)
+   * @see thrust.entities.about.Fuelable#fuel()
+   */
+  public int fuel() {
+    return my_fuel;
+  }
+
+  /* (non-Javadoc)
+   * @see thrust.entities.about.Fuelable#maximum_fuel()
+   */
+  public int maximum_fuel() {
+    return my_max_fuel;
+  }
+
+  /* (non-Javadoc)
+   * @see thrust.entities.about.Fuelable#set_fuel_content(int)
+   */
+  public void set_fuel_content(final int the_fuel_content) {
     my_fuel = the_fuel_content;
   }
   /*@ public invariant (* A fuel pod is destroyed by a bullet. *);
