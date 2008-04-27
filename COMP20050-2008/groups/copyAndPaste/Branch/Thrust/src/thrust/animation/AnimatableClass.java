7bf0244e0c28226a835efbfa1a94424bb2e31e55
pNevin <pNevin@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 14:24:09 +0000

    Started on these, till find out what to do with DynamicEntity

---
+++ COMP20050-2008/groups/copyAndPaste/Branch/Thrust/src/thrust/animation/AnimatableClass.java
@@ -0,0 +1,44 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+
+package thrust.animation;
+
+/**
+ * An entity that moves or changes independent of the player via an animation.
+ * @author Patrick Nevin (patrick.nevin@ucdconnect.ie)
+ * @version 18 April 2008
+ */
+public class AnimatableClass {
+  /**An instance of the Animation class.
+   * 
+   */
+  private Animation my_animation = new AnimationClass();
+  /**
+   * @return What is your animation?
+   */
+  public /*@ pure @*/ Animation animation() {
+    return this.my_animation;
+  }
+
+  /**
+   * @param the_animation This is your animation.
+   */
+  //@ ensures animation() == the_animation;
+  public void animation(Animation the_animation) {
+    this.my_animation = the_animation;
+  }
+
+  /**
+   * Take a next animation step.
+   */
+  public void animate() {
+    //do something probably with an insance g of Graphics
+  }
+}
