86e434fe5f62a0203c9bd719098ebc6f234c35e1
nickm <nickm@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 18:22:11 +0000

    Modified Physics2

--- COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/physics/Physics2.java
+++ COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/physics/Physics2.java
@@ -9,7 +9,7 @@ public class Physics2 implements PhysicsInterface {
   /** Array holding acceleration and orientation of Entity. */
   private double[] my_acceleration;
   /** Double holding the gravitational constant value. */
-  private double my_gravitational_constant;
+  private final double my_gravitational_constant = 0;
   /** Double holding current mass of the ship. */
   private double my_mass;
   /** Double holding current momentum of the Entity. */
@@ -21,8 +21,9 @@ public class Physics2 implements PhysicsInterface {
   /** Array of doubles holding velocity and orientation of Entity. */
   private double[] my_velocity;
   /** Int for use in my_acceleration, my_position and my_velocity arrays. */
-  private static final int MY_ARRAYLENGTH = 2;
-
+  private final int my_arraylength = 2;
+  /** Double holding amount of seconds to simulate for. */
+  private double my_seconds;
 
 
   /** Returns current acceleration of the Entity. */
@@ -31,7 +32,6 @@ public class Physics2 implements PhysicsInterface {
   }
 /** Returns the gravitational constant in use. */
   public double gravitational_constant() {
-    my_gravitational_constant = 0;
     return my_gravitational_constant;
   }
   /** Returns current mass of the Entity in kilograms. */
@@ -40,6 +40,7 @@ public class Physics2 implements PhysicsInterface {
   }
 /** Returns momentum of the Entity in kilograms*meters per second. */
   public double momentum() {
+    my_momentum = my_mass * my_velocity[0]; 
     return my_momentum;
   }
 
@@ -59,41 +60,54 @@ public class Physics2 implements PhysicsInterface {
 
 
   /** Calculates new acceleration of Entity. */
-  public void acceleration(double[] the_acceleration) {
- // All it says in the PhysicsInterface is that this method ensures 
- // my_acceleration == the_acceleration, but where does the_acceleration 
+  public void acceleration(final double[] the_acceleration) {
+
+ // All it says in the PhysicsInterface is that this method ensures
+ // my_acceleration == the_acceleration, but where does the_acceleration
  // array come from, or does that come later?
-    my_acceleration = new double[MY_ARRAYLENGTH];
+
+    my_acceleration = new double[my_arraylength];
     my_acceleration[0] = the_acceleration[0];
     my_acceleration[1] = the_acceleration[1];
-
   }
 
   /** Ensures mass is positive, equal to the_mass. */
-  public void mass(double the_mass) {
+  public void mass(final double the_mass) {
 
+    if (the_mass < 0) {
+      System.out.print("Mass cannot be negative.");
+    } else {
+      my_mass = the_mass;
+    }
   }
 
 /** Ensures orientatian is equal to the_orientation. */
-  public void orientation(double the_orientation) {
-    // TODO Auto-generated method stub
+  public void orientation(final double the_orientation) {
 
+    my_orientation = the_orientation;
   }
 
 /** Ensures positions match up, the_position.length must equal 2. */
-  public void position(double[] the_position) {
-    // TODO Auto-generated method stub
+  public void position(final double[] the_position) {
 
+    my_position = new double[my_arraylength];
+    my_position[0] = the_position[0];
+    my_position[1] = the_position[1];
   }
 
+  // What are the two values stored in the the_velocity array and in what
+  // order do they come?
 /** Ensures velocities match up, the_velocity.length must equal 2. */
-  public void velocity(double[] the_velocity) {
-    // TODO Auto-generated method stub
+  public void velocity(final double[] the_velocity) {
 
+    my_velocity = new double[my_arraylength];
+    my_velocity[0] = the_velocity[0];
+    my_velocity[1] = the_velocity[1];
   }
 
-  public void simulate(double some_seconds) {
-    // TODO Auto-generated method stub
+  public void simulate(final double some_seconds) {
 
+    my_seconds = some_seconds;
   }
+
 }
