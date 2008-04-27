b8b7598ba9b40d97db545c8ead1b75edfdc49241
pNevin <pNevin@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 13:27:55 +0000

    lol these are abstract classes, so provided some implementation instead of creating specific subclasses

--- COMP20050-2008/groups/copyAndPaste/Branch/Thrust/src/thrust/entities/StaticEntity.java
+++ COMP20050-2008/groups/copyAndPaste/Branch/Thrust/src/thrust/entities/StaticEntity.java
@@ -10,15 +10,40 @@
 
 package thrust.entities;
 
+import thrust.physics.PhysicsClass;
+
 /**
  * Entities whose position and orientation do not change.
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 18 April 2008
+ * @revised P Nevin (patrick.nevin@ucdconnect.ie)
+ * @version 27 April 2008
  */
 public abstract class StaticEntity extends DynamicEntity {
-  //@ public model boolean initialized;
+//@ public model boolean initialized;
   //@ public initially initialized == false;
-
+  /**
+   * flag to indicate if instance of object is initialized.
+   */
+  private boolean my_initialized; //no need to assign, false by default
+  /**
+   * create an instance of physics.
+   */
+  private PhysicsClass my_physics = new PhysicsClass();
+  /**
+   * static velocity.
+   */
+  private double[] my_vel = {0, 0};
+  /**
+   * static acceleration.
+   */
+  private double[] my_acc = {0, 0};
+  /**
+   * @return whether instances of this object are initialized
+   */
+  public boolean is_initialized() {
+    return this.my_initialized;
+  }
   /**
    * Set the position and orientation of this entity.  You may only
    * call this method once ever per StaticEntity object.
@@ -30,52 +55,39 @@ public abstract class StaticEntity extends DynamicEntity {
   //@ ensures position()[1] == the_position[1];
   //@ ensures orientation() == the_orientation;
   //@ ensures initialized;
-  public void set_state(double[] the_position,
-                        double the_orientation) {
-    assert false; //@ assert false;
+  public void set_state(final double[] the_pos, final double the_ori) {
+    my_physics.position(the_pos);
+    my_physics.orientation(the_ori);
+    my_initialized = true;
   }
-
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#mass()
    */
   //@ also ensures \result == 0;
   public double mass() {
-    assert false; //@ assert false;
     return 0;
   }
-
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#velocity()
    */
   //@ also ensures \result[0] == 0 & \result[1] == 0;
   public double[] velocity() {
-    assert false; //@ assert false;
-    return null;
+    return my_vel;
   }
-
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#acceleration()
    */
   //@ also ensures \result[0] == 0 & \result[1] == 0;
   public double[] acceleration() {
-    assert false; //@ assert false;
-    return null;
+    return my_acc;
   }
-
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#momentum()
    */
   //@ also ensures \result == 0;
   public double momentum() {
-    assert false; //@ assert false;
     return 0;
   }
 
-  //@ public invariant (* All queries are constant. *);
-  //@ public constraint initialized ==> (position() == \old(position()));
-  //@ public constraint initialized ==> (orientation() == \old(orientation()));
-
-  /*@ public invariant (* Mass, velocity, acceleration, and momentum
-    @                     are all zero. *);
-    @*/
 }
+
