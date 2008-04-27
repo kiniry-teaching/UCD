f5ad39bf901fff71fa00ec505c88c2a053bfe5ba
nickm <nickm@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-27 20:31:01 +0000

    Updating Entity class

--- COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/entities/Entity.java
+++ COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/entities/Entity.java
@@ -12,49 +12,50 @@ package thrust.entities;
 
 import thrust.entities.properties.GameColor;
 import java.awt.Shape;
+import java.awt.Color;
 
 /**
  * Any entity in the game that is drawn in space or on the terrain.
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 18 April 2008
  */
-
 public abstract class Entity implements GameColor {
 
- /** Name of current shape. */
-  private static String my_current_shape_name;
- /** Type of current shape. */
-  private static Shape my_current_shape;
- /** Current state of Entity. */
-  private static byte my_current_state;
+  /** String to hold this Shape's name. */
+  private String my_shape_name;
+  /** Shape to hold this Shape's shape. */
+  private Shape my_shape;
+  /** Byte to hold this Shape's initial state. */
+  private byte my_state;
+  /** Color to hold this Shape's colour. */
+  private Color my_color;
 
   /**
+   * Set the initial shape name, shape, and state of this entity.
    * @param the_initial_shape_name the initial shape name.
    * @param the_initial_shape the initial shape.
    * @param the_initial_state the initial state.
    */
-  public static Entity make(final String the_initial_shape_name,
-                            final Shape the_initial_shape,
-                            final byte the_initial_state) {
-
-    my_current_shape_name = the_initial_shape_name;
-    my_current_shape = the_initial_shape;
-    my_current_state = the_initial_state;
-
+  public void set_state(final String the_initial_shape_name,
+                        final Shape the_initial_shape,
+                        final byte the_initial_state) {
+    my_shape_name = the_initial_shape_name;
+    my_shape = the_initial_shape;
+    my_state = the_initial_state;
   }
 
   /**
    * @return What shape are you?
    */
   public /*@ pure @*/ String shape_name() {
-    return my_current_shape_name;
+    return my_shape_name;
   }
 
   /**
    * @return What shape are you?
    */
   public /*@ pure @*/ Shape shape() {
-    return my_current_shape;
+    return my_shape;
   }
 
   /**
@@ -62,7 +63,7 @@ public abstract class Entity implements GameColor {
    * @param the_shape the shape of this Entity.
    */
   public void shape(final Shape the_shape) {
-    my_current_shape = the_shape;
+    my_shape = the_shape;
   }
 
   /**
@@ -71,7 +72,7 @@ public abstract class Entity implements GameColor {
    */
   //@ ensures 0 <= \result;
   public /*@ pure @*/ byte state() {
-    return my_current_state;
+    return my_state;
   }
 
   /**
@@ -81,11 +82,21 @@ public abstract class Entity implements GameColor {
   //@ requires 0 <= the_state;
   //@ ensures state() == the_state;
   public void state(final byte the_state) {
-    my_current_state = the_state;
+    my_state = the_state;
+  }
+
+  public Color color() {
+    return my_color;
+  }
+
+  public void Color(final Color the_color) {
+    my_color = the_color;
   }
 
   /**
    * Render yourself.
    */
-  public abstract void render();
+  public void render() {
+    assert false;
+  }
 }
