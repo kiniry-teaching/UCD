949d5db82f487226d8c891f398a0bae5fb08dd05
Ccasey <Ccasey@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-28 16:48:17 +0000

    Implemented missing Factory methods

--- COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/Factory.java
+++ COMP20050-2008/groups/kode_kings/Thrust/src/thrust/entities/in_game/Factory.java
@@ -9,24 +9,48 @@
  */
 package thrust.entities.in_game;
 
+import java.util.logging.Logger;
+
 import thrust.animation.Animatable;
+import thrust.animation.Animation;
 import thrust.entities.EnemyEntity;
 import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
+import thrust.entities.behaviors.AI;
 
 /**
  * An enemy factory.
  * @author Ciaran Hale (ciaran.hale@ucdconnect.ie)
- * @version 18 April 2008
+ * @author Colin Casey (colin.casey@org.com)
+ * @version 28 April 2008
  */
 public class Factory extends StaticEntity
   implements EnemyEntity, Animatable {
+  /*@ public invariant (* All factories have exactly one sphere and
+  @                     one chimney. *);
+  @ public invariant (* A bullet causes 1 unit of damage. *);
+  @ public invariant (* Each second 1 unit of damage is eliminated. *);
+  @ public initially (* A factory initially has zero units of damage. *);
+  @ public initially damage() == 0;
+  @ public invariant (* A factory can sustain 20 units of damage before
+  @                     it is destroyed. *);
+  @ public invariant (* A factory with more than 10 units of damage
+  @                     has a chimney that does not smoke. *);
+  @ public invariant 10 < damage() ==> !chimney().smoking();
+  @ public invariant (* A factory with at most 10 units of damage has
+  @                     a smoking chimney. *);
+  @ public invariant damage() <= 10 ==> chimney().smoking();
+  @*/
+
+//@ public invariant (* See constraint on color in FactoryChimney. *);
+//@ public invariant color() == chimney().color();
+
   /** Amount of damage taken by factory. */
-  private byte my_damage_status;
+  private transient byte my_damage_status;
   /** This factories chimney. */
-  private FactoryChimney my_chimney;
+  private transient FactoryChimney my_chimney;
   /** This factories sphere. */
-  private FactorySphere my_sphere;
+  private transient FactorySphere my_sphere;
 
   /**
    * @return How much damage have you sustained?
@@ -50,45 +74,74 @@ public class Factory extends StaticEntity
     return my_sphere;
   }
 
-
   /**
    * @param the_damage_staus You have taken this many units of damage.
    */
   //@ requires 0 <= the_damage;
   //@ ensures damage() == \old(damage() - the_damage);
   public void damage(final byte the_damage) {
-    my_damage_status += the_damage;
+    my_damage_status -= the_damage;
   }
 
-  /*@ public invariant (* All factories have exactly one sphere and
-    @                     one chimney. *);
-    @ public invariant (* A bullet causes 1 unit of damage. *);
-    @ public invariant (* Each second 1 unit of damage is eliminated. *);
-    @ public initially (* A factory initially has zero units of damage. *);
-    @ public initially damage() == 0;
-    @ public invariant (* A factory can sustain 20 units of damage before
-    @                     it is destroyed. *);
-    @ public invariant (* A factory with more than 10 units of damage
-    @                     has a chimney that does not smoke. *);
-    @ public invariant 10 < damage() ==> !chimney().smoking();
-    @ public invariant (* A factory with at most 10 units of damage has
-    @                     a smoking chimney. *);
-    @ public invariant damage() <= 10 ==> chimney().smoking();
-    @*/
+  public AI attack() {
+    return my_chimney.attack();
+  }
 
-  //@ public invariant (* See constraint on color in FactoryChimney. *);
-  //@ public invariant color() == chimney().color();
+  public AI disturb() {
+    return my_chimney.disturb();
+  }
+
+  public void attack(final AI the_behavior) {
+    my_chimney.attack(the_behavior);
+  }
+
+  public void disturb(final AI the_behavior) {
+    my_chimney.disturb(the_behavior);
+  }
+
+  public Animation animation() {
+    return my_chimney.animation();
+  }
+
+  public void animation(final Animation the_animation) {
+    my_chimney.animation(the_animation);
+  }
+
+  public void animate() {
+    my_chimney.animate();
+  }
 
   /**
    * A chimney of a factory.
    * @author Ciaran Hale (ciaran.hale@ucdconnect.ie)
-   * @version 18 April 2008
+   * @author Colin Casey (colin.casey@org.com)
+   * @version 28 April 2008
    */
   public class FactoryChimney extends StaticEntity
     implements EnemyEntity, Animatable {
 
-    /** . */
+    /*@ public invariant (* A factories chimney is the same color as
+    @                     its factory. *);
+    @ public invariant (* The goal sphere is destroyed by a
+    @                     factory's chimney. *);
+    @ public invariant (* The spaceship is destroyed by a factory's
+    @                     chimney. *);
+    @*/
+
+    /** Whether the FactoryChimney is smoking or not. */
     private boolean my_smoking_state;
+    /** The attack AI of a Factory. */
+    private transient AI my_attack_ai;
+    /** The disturb AI of a Factory. */
+    private transient AI my_disturb_ai;
+    /** Logger for animation. */
+    private final Logger my_logger =
+      Logger.getLogger(Explosion.class.getName());
+    /** The frames in the Star animation. */
+    private transient Animation my_animation;
+    /** Animation frame counter. */
+    private transient int my_animation_counter;
+
     /**
      * @return Are you smoking?
      */
@@ -106,13 +159,41 @@ public class Factory extends StaticEntity
       my_smoking_state = the_smoking_state;
     }
 
-    /*@ public invariant (* A factories chimney is the same color as
-      @                     its factory. *);
-      @ public invariant (* The goal sphere is destroyed by a
-      @                     factory's chimney. *);
-      @ public invariant (* The spaceship is destroyed by a factory's
-      @                     chimney. *);
-      @*/
+    public AI attack() {
+      return my_attack_ai;
+    }
+
+    public AI disturb() {
+      return my_disturb_ai;
+    }
+
+    public void attack(final AI the_behavior) {
+      my_attack_ai = the_behavior;
+    }
+
+    public void disturb(final AI the_behavior) {
+      my_disturb_ai = the_behavior;
+    }
+
+    public Animation animation() {
+      return my_animation;
+    }
+
+    public void animation(final Animation the_animation) {
+      my_animation = the_animation;
+    }
+
+    public void animate() {
+      /* When animate is called a frame of animation is played
+       * Resets after FactoryChimney animation completes
+       */
+      my_animation_counter++;
+      my_logger.fine("Factory Chimney animation step " +
+                       my_animation_counter + " has been rendered.");
+      if (my_animation_counter % 10 == 0) {
+        my_animation_counter = 0;
+      }
+    }
   }
 
   /**
