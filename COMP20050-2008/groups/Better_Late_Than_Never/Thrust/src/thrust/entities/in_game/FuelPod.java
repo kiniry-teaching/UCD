2b337b30f94eb7c42b572c8baae5330c8968f48d
nickm <nickm@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-28 14:06:13 +0000

    Implemented Fuelable methods for FuelPod class.

--- COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/entities/in_game/FuelPod.java
+++ COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/entities/in_game/FuelPod.java
@@ -20,6 +20,67 @@ import thrust.entities.about.Fuelable;
  */
 public class FuelPod extends StaticEntity
   implements NeutralEntity, Fuelable {
+
+  /** Int holding maximum amount of fuel in the Fuelpod. */
+  private static final int MY_MAXFUEL = 500;
+  /** Int holding current amount of fuel in the Fuelpod. */
+  private static int my_fuel;
+  /** Int holding mass of fuel. */
+  private static int my_fuel_mass = my_fuel;
+
+  /**
+   * @return How much fuel do you contain?
+   */
+  //@ ensures 0 <= \result;
+  //@ ensures \result <= maximum_fuel();
+  public /*@ pure @*/ int fuel() {
+    return my_fuel;
+  }
+
+  /**
+   * @return How much fuel can you contain?
+   */
+  //@ ensures 0 <= \result;
+  public /*@ pure @*/ int maximum_fuel() {
+    return MY_MAXFUEL;
+  }
+  
+  //@ public invariant (* One unit of fuel weights 1kg. *);
+  /**
+   * @return What is the mass of your fuel?
+   */
+  //@ ensures \result == fuel * 1;
+  public /*@ pure @*/ int fuel_mass() {
+    return my_fuel_mass;
+  }
+
+  /**
+   * @param the_fuel_content This many units is your fuel content.
+   */
+  //@ requires 0 <= the_fuel_content & the_fuel_content <= maximum_fuel();
+  //@ ensures fuel() == the_fuel_content;
+  public void set_fuel_content(final int the_fuel_content) {
+    my_fuel = the_fuel_content;
+  }
+
+  /**
+   * @param the_fuel_change Change your fuel content by this many units.
+   */
+  /*@ ensures (\old(fuel() + the_fuel_change < 0) ?
+    @            (fuel() == 0) :
+    @          (\old(maximum_fuel() < (fuel() + the_fuel_change)) ?
+    @             (fuel() == maximum_fuel()) :
+    @           fuel() == \old(fuel() + the_fuel_change)));
+    @*/
+  public void change_fuel_content(final int the_fuel_change) {
+    my_fuel += the_fuel_change; // Clumsy, fix in future.
+  }
+
+  //@ public invariant (* Fuel content is always non-negative and finite. *);
+  //@ public invariant 0 <= fuel();
+
+
+
   /*@ public invariant (* A fuel pod is destroyed by a bullet. *);
     @ public invariant (* The fuel pod is not affected by the goal sphere. *);
     @ public invariant (* The fuel pod is not affected by the spaceship. *);
