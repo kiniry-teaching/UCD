b9e2bab79833461223a693c8f0e90e310cbae217
PeterGibney <PeterGibney@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-29 15:50:05 +0000

    Files moved to selfCheckOut, their correct place

---
+++ COMP30050-2008/groups/SelfCheckOut/Peter/CodeTests/HardWareConcept01/RunBC.bat
@@ -0,0 +1,3 @@
+@if exist C:\software.key\sync.exe C:\software.key\sync
+@java -ea ShowBarCode 127.0.0.1
+@if exist C:\software.key\sync.exe C:\software.key\sync
---
+++ COMP30050-2008/groups/SelfCheckOut/Peter/CodeTests/HardWareConcept01/ScalesServer.bat
@@ -0,0 +1,3 @@
+@if exist C:\software.key\sync.exe C:\software.key\sync
+java -classpath .;Phidget21.jar -ea ScalesServer3
+@if exist C:\software.key\sync.exe C:\software.key\sync
