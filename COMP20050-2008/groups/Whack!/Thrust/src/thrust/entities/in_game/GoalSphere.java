dc461e82d8c056f871dcc8b593d53152b96ac052
Tara <Tara@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-29 16:14:02 +0000

    update

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/GoalSphere.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/GoalSphere.java
@@ -9,6 +9,8 @@
  */
 package thrust.entities.in_game;
 
+import java.awt.Color;
+
 import thrust.entities.DynamicEntity;
 import thrust.entities.NeutralEntity;
 import thrust.entities.behaviors.Tow;
@@ -16,11 +18,11 @@ import thrust.entities.behaviors.Tow;
 /**
  * The goal sphere that the spaceship needs to tow into
  * space away from the terrain to escape.
- * @author Joe Kiniry (kiniry@acm.org)
- * @version 18 April 2008
+ * @author Tara Flood (Tara.Flood@ucdconnect.ie)
+ * @version 29 April 2008
  */
 public class GoalSphere extends DynamicEntity
-  implements NeutralEntity, Tow {
+implements NeutralEntity, Tow {
   /*@ public invariant (* The fuel pod is destroyed by a bullet. *);
     @ public invariant (* If the fuel pod is destroyed, the spaceship
     @                     is destroyed. *);
@@ -45,4 +47,105 @@ public class GoalSphere extends DynamicEntity
    * The mass of the goal sphere is 10,000kg.
    */
   public static final int MASS = 10000;
+  /**
+   * DynamicEntity.
+   */
+  DynamicEntity my_dynamicentity;
+  /**
+   * Velocity.
+   */
+  double[] my_velocity;
+  /**
+   * sphere color.
+   */
+  Color my_spherecolor;
+  /**
+   * time.
+   */
+  double my_time;
+  /**
+   * position.
+   */
+  double [] my_position;
+  /**
+   * orientation.
+   */
+  double my_orientation;
+  /**
+   * mass.
+   */
+  double my_mass;
+  public void tow() {
+    // TODO Auto-generated method stub
+  }
+
+  public boolean towed() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public double[] acceleration() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void acceleration(final double[] the_acceleration) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public double gravitational_constant() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double mass() {
+    my_mass = 1;
+    return my_mass;
+  }
+
+  public void mass(final double the_mass) {
+    my_dynamicentity.mass(the_mass);
+  }
+
+
+  public double momentum() {
+    return my_dynamicentity.momentum();
+  }
+
+  public double orientation() {
+    return my_orientation;
+  }
+
+  public void orientation(final double the_orientation) {
+    my_dynamicentity.orientation(the_orientation);
+
+  }
+  public double[] position() {
+    return  my_position;
+  }
+
+  public void position(final double[] the_position) {
+    my_dynamicentity.position(the_position);
+  }
+
+  public void simulate(final double some_seconds) {
+    my_time = some_seconds;
+  }
+
+  public double[] velocity() {
+    return my_velocity;
+  }
+
+  public void velocity(final double[] the_velocity) {
+    my_dynamicentity.velocity(the_velocity);
+  }
+  public Color color() {
+    my_spherecolor.equals(Color.GREEN);
+    return my_spherecolor;
+  }
+
+  public void color(final Color the_color) {
+    my_spherecolor = the_color;
+  }
 }
