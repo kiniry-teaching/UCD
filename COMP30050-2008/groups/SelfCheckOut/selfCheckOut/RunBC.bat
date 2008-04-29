b8a53e7d98b5db0d6d55282228ebcb5ea4e271a7
PeterGibney <PeterGibney@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-29 18:41:29 +0000

    Deleted files from Eclipse crash.  All the code is now in the foder src

--- COMP30050-2008/groups/SelfCheckOut/selfCheckOut/RunBC.bat
+++
@@ -1,3 +0,0 @@
-@if exist C:\software.key\sync.exe C:\software.key\sync
-@java -ea selfCheckOut.ProcessHardWare 127.0.0.1
-@if exist C:\software.key\sync.exe C:\software.key\sync
--- COMP30050-2008/groups/SelfCheckOut/selfCheckOut/ScalesServer.bat
+++
@@ -1,3 +0,0 @@
-@if exist C:\software.key\sync.exe C:\software.key\sync
-java -classpath .;Phidget21.jar -ea selfCheckOut.ScalesServer
-@if exist C:\software.key\sync.exe C:\software.key\sync
