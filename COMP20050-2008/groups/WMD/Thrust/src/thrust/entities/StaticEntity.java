8a4e732c88a39d5d26af0612e3d954d7dbc01f3e
prometheos <prometheos@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-29 15:06:27 +0000

    Render yourself...wtf?

--- COMP20050-2008/groups/WMD/Thrust/src/thrust/entities/StaticEntity.java
+++ COMP20050-2008/groups/WMD/Thrust/src/thrust/entities/StaticEntity.java
@@ -1,5 +1,4 @@
 
-
 package thrust.entities;
 
 import java.awt.Shape;
@@ -18,16 +17,17 @@ public abstract class StaticEntity extends DynamicEntity {
   //@ ensures position().equals(the_position);
   //@ ensures orientation().equals(the_orientation);
   public void set_Staticstate (final double[] the_position,
-                              final double the_orientation,
-                              final double[] the_acceleration,
-                              final double the_mass,
-                              final double[] the_velocity,
-                              final String the_initial_shape_name,
-                              final Shape the_initial_shape,
-                              final byte the_initial_state) {
+                               final double the_orientation,
+                               final double[] the_acceleration,
+                               final double the_mass,
+                               final double[] the_velocity,
+                               final String the_initial_shape_name,
+                               final Shape the_initial_shape,
+                               final byte the_initial_state) {
 
-    super.set_State(the_initial_shape_name,
-                    the_initial_shape, the_initial_state);
+    super.set_Dynamic_State(the_position, the_orientation, the_acceleration,
+                            the_mass, the_velocity, the_initial_shape_name,
+                            the_initial_shape, the_initial_state);
 
   }
 
@@ -35,31 +35,44 @@ public abstract class StaticEntity extends DynamicEntity {
    * @see thrust.physics.PhysicsInterface#mass()
    */
   //@ ensures \result == 0;
-  public abstract double mass();
+  public double mass() {
+    final double actualMass = 0;
+    return actualMass;
+  }
 
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#velocity()
    */
   //@ ensures \result[0] == 0 & \result[1] == 0;
-  public abstract double[] velocity();
+  public double[] velocity() {
+    final double[] vel = {0, 0};
+    return vel;
+  }
 
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#acceleration()
    */
   //@ ensures \result[0] == 0 & \result[1] == 0;
-  public abstract double[] acceleration();
+  public double[] acceleration() {
+    final double[] acc = {0, 0};
+    return acc;
+  }
 
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#momentum()
    */
   //@ ensures \result == 0;
-  public abstract double momentum();
+  public double momentum() {
+    final double mom = 0;
+    return mom;
+  }
 
   //@ public invariant (* All queries are constant. *);
   //@ public constraint position() == \old(position());
   //@ public constraint orientation() == \old(orientation());
 
   /*@ public invariant (* Mass, velocity, acceleration, and momentum
-    @                     are all zero. *);
-    @*/
+      @                     are all zero. *);
+      @*/
 }
+
