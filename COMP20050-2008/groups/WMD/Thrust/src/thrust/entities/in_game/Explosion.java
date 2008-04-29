50cccfe3eeedf0a6a134bbda43e152433908342e
sivie <sivie@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-29 09:49:20 +0000

    entities in game

---
+++ COMP20050-2008/groups/WMD/Thrust/src/thrust/entities/in_game/Explosion.java
@@ -0,0 +1,119 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+package thrust.entities.in_game;
+
+import java.awt.Color;
+import java.awt.Shape;
+
+import thrust.entities.NeutralEntity;
+import thrust.entities.StaticEntity;
+import thrust.entities.behaviors.AI;
+import thrust.animation.Animatable;
+import thrust.animation.Animation;
+
+/**
+ * An explosion.
+ * @author Siobhan Dunne (Siobhan.Dunne@ucd.ie)
+ * @version 24 April 2008
+ */
+public class Explosion extends StaticEntity
+  implements NeutralEntity, Animatable {
+
+  public double[] acceleration() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double mass() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double momentum() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] velocity() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void render() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Shape shape() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void shape(Shape the_shape) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public String shape_name() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public byte state() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void state(byte the_state) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void animate() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Animation animation() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void animation(Animation the_animation) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public double gravitational_constant() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double orientation() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] position() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Color color() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void color(Color the_color) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
---
+++ COMP20050-2008/groups/WMD/Thrust/src/thrust/entities/in_game/FuelPod.java
@@ -0,0 +1,108 @@
+/*
+ * A re-implementation of the classic C=64 game 'Thrust'.
+ *
+ * @author "Joe Kiniry (kiniry@acm.org)"
+ * @module "COMP 20050, COMP 30050"
+ * @creation_date "March 2007"
+ * @last_updated_date "April 2008"
+ * @keywords "C=64", "Thrust", "game"
+ */
+package thrust.entities.in_game;
+
+import java.awt.Color;
+import java.awt.Shape;
+
+import thrust.entities.NeutralEntity;
+import thrust.entities.StaticEntity;
+import thrust.entities.about.Fuelable;
+
+/**
+ * A fuel pod from which the spaceship can obtain fuel.
+ * @author Siobhan Dunne (Siobhan.Dunne@ucd.ie)
+ */
+public class FuelPod extends StaticEntity
+  implements NeutralEntity, Fuelable {
+
+  public double[] acceleration() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double mass() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double momentum() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] velocity() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void render() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Shape shape() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void shape(Shape the_shape) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public String shape_name() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public byte state() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void state(byte the_state) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public double gravitational_constant() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double orientation() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public double[] position() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Color color() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void color(Color the_color) {
+    // TODO Auto-generated method stub
+    
+  }
+  /*@ public invariant (* A fuel pod is destroyed by a bullet. *);
+    @ public invariant (* The fuel pod is not affected by the goal sphere. *);
+    @ public invariant (* The fuel pod is not affected by the spaceship. *);
+    @ public invariant (* A fuel pod's color is always yellow. *);
+    @ public invariant color() == thrust.entities.properties.GameColor.YELLOW;
+    @ public invariant (* A fuel pod's 'fuel' lettering color is
+    @                     dictated by the amount of fuel it contains. *);
+    @*/
+}
