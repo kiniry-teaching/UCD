137841d121a3e94d5fb416bd2eeaeeb8acb2fe15
nickm <nickm@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-30 23:53:12 +0000

    constructor for infopanel

--- COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/display/InfoPanel.java
+++ COMP20050-2008/groups/Better_Late_Than_Never/Thrust/src/thrust/display/InfoPanel.java
@@ -11,24 +11,34 @@ public class InfoPanel extends AbstractInfoPanel {
 
   /** Boolean holding current displayed state of InfoPanel. */
   private static boolean my_display_state;
+  /** Int holding current lives. */
+  private static int my_lives;
+  /** Int holding current fuel level. */
+  private static int my_fuel;
+  /** Int holding current score. */
+  private static int my_score;
 
-
+  public InfoPanel(final int the_current_lives,
+                   final int the_current_fuel,
+                   final int the_current_score) {
+    super();
+    my_lives = the_current_lives;
+    my_fuel = the_current_fuel;
+    my_score = the_current_score;
+  }
+  // Display methods.
   public boolean displayed() {
     return my_display_state;
   }
-
   public void display() {
     my_display_state = true;
-
   }
-
   public void hide() {
     my_display_state = false;
-
   }
 
   public void update() {
-    my_display_state.add_new_high_score(new_high_score);
+    // ../.?...?.?? 
   }
 
 }
