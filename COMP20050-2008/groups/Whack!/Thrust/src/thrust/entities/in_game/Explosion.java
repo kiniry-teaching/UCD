a6b1dca2b2c67a16022d41998965ad35de4b105b
Tara <Tara@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-04-30 11:03:24 +0000

    Explosion

--- COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/Explosion.java
+++ COMP20050-2008/groups/Whack!/Thrust/src/thrust/entities/in_game/Explosion.java
@@ -9,16 +9,119 @@
  */
 package thrust.entities.in_game;
 
+import java.awt.Color;
+
 import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
 import thrust.animation.Animatable;
+import thrust.animation.Animation;
 
 /**
  * An explosion.
  * @author Joe Kiniry (kiniry@acm.org)
  * @version 18 April 2008
  */
+
 public class Explosion extends StaticEntity
-  implements NeutralEntity, Animatable {
+implements NeutralEntity, Animatable {
+  /**
+   * Static Entity.
+   */
+  StaticEntity my_staticentity;
+  /**
+   * acceleration.
+   */
+  double[] my_acceleration;
+  /**
+   * orientation.
+   */
+  double my_orientation;
+  /**
+   * position.
+   */
+  double[] my_position;
+  /**
+   * time.
+   */
+  double my_time;
+  /**
+   * velocity.
+   */
+  double[] my_velocity;
+  /**
+   * mass.
+   */
+  double my_mass;
+  /**
+   * Color.
+   */
+  Color my_explosioncolor;
+
+  public void animate() {
+    // TODO Auto-generated method stub
+  }
+
+  public Animation animation() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void animation(final Animation the_animation) {
+    // TODO Auto-generated method stub
+
+  }
+  public double[] acceleration() {
+    return my_acceleration;
+  }
+
+  public void acceleration(final double[] the_acceleration) {
+    my_staticentity.acceleration();
+  }
+
+  public double gravitational_constant() {
+    return my_staticentity.gravitational_constant();
+  }
+  public double mass() {
+    my_mass = 1;
+    return my_mass;
+  }
+
+  public void mass(final double the_mass) {
+    my_staticentity.mass(the_mass);
+
+  }
+
+  public double orientation() {
+    return my_orientation;
+  }
+
+  public void orientation(final double the_orientation) {
+    my_staticentity.orientation(the_orientation);
+  }
+
+  public double[] position() {
+    return my_position;
+  }
+
+  public void position(final double[] the_position) {
+    my_staticentity.position(the_position);
+  }
+
+  public void simulate(final double some_seconds) {
+    my_time = some_seconds;
+  }
+
+  public void velocity(final double[] the_velocity) {
+    my_staticentity.velocity(the_velocity);
+  }
+
+  public Color color() {
+    my_explosioncolor.equals(Color.GREEN);
+    return my_explosioncolor;
+  }
+
+  public void color(final Color the_color) {
+    my_explosioncolor = the_color;
+  }
 
 }
