5ba0a6f1e7829edde7d4e3b75fb991b9365151f5
cykio <cykio@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-05-01 16:30:56 +0000

    Added FuelEntiry class

--- COMP20050-2008/groups/Thick,_Dapper_Deviants/Thrust/src/thrust/entities/about/FuelEntity.java
+++ COMP20050-2008/groups/Thick,_Dapper_Deviants/Thrust/src/thrust/entities/about/FuelEntity.java
@@ -1,36 +1,48 @@
 package thrust.entities.about;
 
 public class FuelEntity implements Fuelable {
+
+
+  /*
+   * @return The current fuel of the object
+   */
   
-  public int fuel(){
-  {
-    if (fuel > 0 && fuel < maximum_fuel){
-      return fuel;
+  private int my_fuel;
+
+  int maximum_fuel = 1000;
+
+  int fuel_weight = 1;
+
+  public int check_fuel(){
+    {
+      if (fuel > 0 && fuel < maximum_fuel) {
+        return fuel;
+      }
+      else if (fuel >= maximum_fuel) {
+        return maximum_fuel;
+      }
+      else if (fuel == 0) {
+        return fuel;
+      }
     }
-    else if (fuel > maximum_fuel){
-      return maximum_fuel;
-    }
-    else if(fuel < 0){
-      return  fuel = 0;
-     }
-  }
-  
   }
 
   public void change_fuel_content(int the_fuel_change) {
     // TODO Auto-generated method stub
-    
   }
 
   public int fuel_mass() {
     // TODO Auto-generated method stub
-    return 0;
+    return fuel * fuel_weight;
   }
 
   public void set_fuel_content(int the_fuel_content) {
     // TODO Auto-generated method stub
-    
   }
-  
+  public int max_fuel() {
+    // TODO Auto-generated method stub
+    return maximum_fuel;
+  }
+
 
 }
