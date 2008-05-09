3d00a802c4f5b734a5e8dc32aee14c5bc9d2df8d
magda <magda@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-05-09 09:13:25 +0000

    less blanks in entities.in_game

--- COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/in_game/Bullet.java
+++ COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/in_game/Bullet.java
@@ -11,6 +11,9 @@ package thrust.entities.in_game;
 
 import thrust.entities.DynamicEntity;
 import thrust.entities.EnemyEntity;
+import thrust.entities.behaviors.AI;
+import thrust.physics.PhysicsClass;
+import thrust.physics.PhysicsInterface;
 
 /**
  * A bullet shot from the spaceship or a gun turret.
@@ -19,13 +22,16 @@ import thrust.entities.EnemyEntity;
  */
 public class Bullet extends DynamicEntity
   implements EnemyEntity {
+  
+  private PhysicsInterface my_physics = new PhysicsClass();
+  
   /* (non-Javadoc)
    * @see thrust.physics.PhysicsInterface#mass()
    */
   //@ ensures \result == 1;
   public double mass() {
     assert false; //@ assert false;
-    return 0;
+    return 1;
   }
 
   /*@ public invariant (* Bullets are destroyed on contact with a
@@ -34,4 +40,86 @@ public class Bullet extends DynamicEntity
     @                     terrain. *);
     @*/
   //@ public invariant (* Bullets have a mass of 1 kg. *);
+  
+  /**
+   * @return What is your acceleration in meters per second squared?
+   */
+  //@ ensures \result.length == 2;
+  public /*@ pure @*/ double[] acceleration(){
+   return my_physics.acceleration();
+  }
+
+  /**
+   * @return What is the gravitational constant?
+   */
+  public /*@ pure @*/ double gravitational_constant(){
+    return my_physics.gravitational_constant();
+  }
+
+
+  /**
+   * @return What is your momentum in kilograms*meters per second?
+   */
+  public /*@ pure @*/ double momentum(){
+    return my_physics.momentum();
+  }
+
+  /**
+   * @return What is your orientation in radians?
+   */
+  public /*@ pure @*/ double orientation(){
+    return my_physics.orientation();
+  }
+
+  /**
+   * @return What is your position in meters from the origin?
+   */
+  //@ ensures \result.length == 2;
+ public  /*@ pure @*/ double[] position(){
+   return my_physics.position();
+  }
+
+  /**
+   * @return What is your velocity in meters per second?
+   */
+ public  /*@ pure @*/ double[] velocity(){
+   return my_physics.velocity();
+  }
+
+  /**
+   * Simulate yourself for this many seconds.
+   * @param some_seconds the number of seconds to simulate.
+   */
+  public void simulate(double some_seconds){
+    my_physics.simulate(some_seconds);
+ }
+  /**
+   * @return What is your attack behavior AI?
+   */
+  public /*@ pure @*/ AI attack(){
+    
+  }
+
+  /**
+   * @return What is your disturb behavior AI?
+   */
+  public /*@ pure @*/ AI disturb(){
+    
+  }
+  /**
+   * @param the_behavior This is your attack behavior.
+   */
+  //@ ensures attack() == the_behavior;
+  public void attack(AI the_behavior){
+    
+  }
+
+  /**
+   * @param the_behavior This is your disturb behavior.
+   */
+  //@ ensures disturb() == the_behavior;
+  public void disturb(AI the_behavior){
+    
+  }
+  
 }
