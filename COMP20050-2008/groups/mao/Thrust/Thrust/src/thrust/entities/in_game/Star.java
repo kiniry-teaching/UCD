cf22c22ffe5a22e1e2f5da683c49cd90d9d312f1
magda <magda@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-05-09 09:14:08 +0000

    less blanks in entities.in_game

--- COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/in_game/Star.java
+++ COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/in_game/Star.java
@@ -10,8 +10,11 @@
 package thrust.entities.in_game;
 
 import thrust.animation.Animatable;
+import thrust.animation.Animation;
 import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
+import thrust.physics.PhysicsClass;
+import thrust.physics.PhysicsInterface;
 
 /**
  * A blinking star in space.
@@ -25,4 +28,87 @@ public class Star extends StaticEntity
     @ public invariant (* Each star blinks irregularly. *);
     @ public invariant (* A star's shape is always a small square. *);
     @*/
+  
+  private PhysicsInterface my_physics = new PhysicsClass();
+  private Animation my_animation;
+  
+  /**
+   * @return What is your animation?
+   */
+  public /*@ pure @*/ Animation animation(){
+    return my_animation;
+  }
+
+  /**
+   * @param the_animation This is your animation.
+   */
+  //@ ensures animation() == the_animation;
+  public void animation(Animation the_animation){
+    
+  }
+
+  /**
+   * Take a next animation step.
+   */
+  public void animate(){
+    
+  }
+  
+  /**
+   * @return What is your acceleration in meters per second squared?
+   */
+  //@ ensures \result.length == 2;
+  public /*@ pure @*/ double[] acceleration(){
+   return my_physics.acceleration();
+  }
+
+  /**
+   * @return What is the gravitational constant?
+   */
+  public /*@ pure @*/ double gravitational_constant(){
+    return my_physics.gravitational_constant();
+  }
+
+  /**
+   * @return What is your mass in kilograms?
+   */
+  //@ ensures 0 <= \result;
+ public /*@ pure @*/ double mass(){
+
+    return my_physics.mass();
+  }
+
+  /**
+   * @return What is your momentum in kilograms*meters per second?
+   */
+  public /*@ pure @*/ double momentum(){
+    return my_physics.momentum();
+  }
+
+  /**
+   * @return What is your orientation in radians?
+   */
+  public /*@ pure @*/ double orientation(){
+    return my_physics.orientation();
+  }
+
+  /**
+   * @return What is your position in meters from the origin?
+   */
+  //@ ensures \result.length == 2;
+ public  /*@ pure @*/ double[] position(){
+   return my_physics.position();
+  }
+ 
+ public void simulate(double time){
+   my_physics.simulate(time);
+ }
+
+  /**
+   * @return What is your velocity in meters per second?
+   */
+ public  /*@ pure @*/ double[] velocity(){
+   return my_physics.velocity();
+  }
+  
 }
--- COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/in_game/Terrain.java
+++ COMP20050-2008/groups/mao/Thrust/Thrust/src/thrust/entities/in_game/Terrain.java
@@ -9,8 +9,11 @@
  */
 package thrust.entities.in_game;
 
+import thrust.animation.Animation;
 import thrust.entities.NeutralEntity;
 import thrust.entities.StaticEntity;
+import thrust.physics.PhysicsClass;
+import thrust.physics.PhysicsInterface;
 
 /**
  * The planet on which some entities rest.
@@ -23,4 +26,64 @@ public class Terrain extends StaticEntity
     @ public invariant (* The shape of the terrain is rendered as a
     @                     sequence of horizontal lines. *);
     @*/
+  
+  private PhysicsInterface my_physics = new PhysicsClass();
+ 
+  /**
+   * @return What is your acceleration in meters per second squared?
+   */
+  //@ ensures \result.length == 2;
+  public /*@ pure @*/ double[] acceleration(){
+   return my_physics.acceleration();
+  }
+
+  /**
+   * @return What is the gravitational constant?
+   */
+  public /*@ pure @*/ double gravitational_constant(){
+    return my_physics.gravitational_constant();
+  }
+
+  /**
+   * @return What is your mass in kilograms?
+   */
+  //@ ensures 0 <= \result;
+ public /*@ pure @*/ double mass(){
+
+    return my_physics.mass();
+  }
+
+  /**
+   * @return What is your momentum in kilograms*meters per second?
+   */
+  public /*@ pure @*/ double momentum(){
+    return my_physics.momentum();
+  }
+
+  /**
+   * @return What is your orientation in radians?
+   */
+  public /*@ pure @*/ double orientation(){
+    return my_physics.orientation();
+  }
+
+  /**
+   * @return What is your position in meters from the origin?
+   */
+  //@ ensures \result.length == 2;
+ public  /*@ pure @*/ double[] position(){
+   return my_physics.position();
+  }
+ 
+ public void simulate(double time){
+   my_physics.simulate(time);
+ }
+
+  /**
+   * @return What is your velocity in meters per second?
+   */
+ public  /*@ pure @*/ double[] velocity(){
+   return my_physics.velocity();
+  }
+  
 }
