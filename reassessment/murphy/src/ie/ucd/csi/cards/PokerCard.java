33c3f1ee3fcdcb6012e0ffc8eb3e731d64b905f2
comurphy <comurphy@65e8e7ae-2caf-4cb0-b31f-ed4b7585fe8d>
2008-11-28 13:14:51 +0000

    first commit

---
+++ reassessment/murphy/src/ie/ucd/csi/cards/PokerCard.java
@@ -0,0 +1,26 @@
+/*
+ * UCD CSI Playing Card System.
+ * $Id: PokerCard.java 1096 2005-08-24 23:02:36Z jkiniry $
+ */
+
+package ie.ucd.csi.cards;
+
+/**
+ * A standard playing card.
+ *
+ * @author Joseph Kiniry <joseph.kiniry@ucd.ie>
+ */
+
+public class PokerCard implements Card {
+
+  public PokerCard(byte suite, byte value) {
+  }
+
+  public String toString() {
+    return null;
+  }
+
+  public boolean equals(Object o) {
+    return false;
+  }
+}
